132c0711196eab7da60b245845b362a8fd427bda
Aditya Singhania <aditya.singhania@sjsu.edu>
2023-03-12 14:50:56 -0700

    adding bundle client app

---
+++ BundleClient/app/src/main/java/com/ddd/bundleclient/HelloworldActivity.java
@@ -0,0 +1,230 @@
+
+package com.ddd.bundleclient;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import com.ddd.wifidirect.WifiDirectManager;
+import com.google.protobuf.ByteString;
+
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.ref.WeakReference;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+public class HelloworldActivity extends AppCompatActivity {
+  // tag used for testing in logcat
+  public static final String TAG = "dddDebug";
+  // Wifi Direct set up
+  private com.ddd.wifidirect.WifiDirectManager wifiDirectManager;
+  public static final int PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION = 1001;
+  private static final int WRITE_EXTERNAL_STORAGE = 1002;
+  // gRPC set up
+  private Button connectButton;
+  private FileChooserFragment fragment;
+  private TextView resultText;
+
+
+  /** check for location permissions manually, will give a prompt*/
+  @Override
+  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                         @NonNull int[] grantResults) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    switch (requestCode) {
+      case PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION:
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+          Log.e(TAG, "Fine location permission is not granted!");
+          finish();
+        }
+        break;
+    }
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // set up view
+    setContentView(R.layout.activity_helloworld);
+    connectButton = (Button) findViewById(R.id.connect_button);
+    resultText = (TextView) findViewById(R.id.grpc_response_text);
+    resultText.setMovementMethod(new ScrollingMovementMethod());
+
+    FragmentManager fragmentManager = this.getSupportFragmentManager();
+    this.fragment = (FileChooserFragment) fragmentManager.findFragmentById(R.id.fragment_fileChooser);
+
+    // set up wifi direct
+    wifiDirectManager = new WifiDirectManager(this.getApplication(), this.getLifecycle());
+
+    connectButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        sendMessage(wifiDirectManager);
+      }
+    });
+  }
+
+
+  public void sendMessage(WifiDirectManager wifiDirectManager) {
+    // connect to transport
+    connectTransport(wifiDirectManager);
+    // check if connection successful, using intents?
+
+    //start grpc task
+
+//    sendButton.setEnabled(false);
+//    resultText.setText("");
+//    new GrpcTask(this)
+//        .execute(
+//                "192.168.49.1",
+//                "7777",
+//                this.fragment.getPath());
+  }
+
+  private class GrpcTask extends AsyncTask<String, Void, String> {
+    private final WeakReference<Activity> activityReference;
+    private ManagedChannel channel;
+
+    private GrpcTask(Activity activity) {
+      this.activityReference = new WeakReference<Activity>(activity);
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+      String host = params[0];
+      String portStr = params[2];
+      String pathStr = params[3];
+      int port = TextUtils.isEmpty(portStr) ? 0 : Integer.valueOf(portStr);
+      try {
+        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        FileServiceGrpc.FileServiceStub stub = FileServiceGrpc.newStub(channel);
+        StreamObserver<FileUploadRequest> streamObserver = stub.uploadFile(new FileUploadObserver());
+
+// input file for testing
+        Path path = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          path = Paths.get(pathStr);
+        }
+//        Log.d("wDebug",path.toString());
+// build metadata
+        Date current = Calendar.getInstance().getTime();
+        FileUploadRequest metadata = FileUploadRequest.newBuilder()
+                .setMetadata(MetaData.newBuilder()
+                        .setName("payload_"+current.getTime())
+                        .setType("zip").build())
+                .build();
+        streamObserver.onNext(metadata);
+
+// upload file as chunk
+        current = Calendar.getInstance().getTime();
+        Log.d("wDebug","Started file transfer");
+        InputStream inputStream = null;
+//        ZipInputStream inputStream = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//          inputStream = Files.newInputStream(path);
+          inputStream = getResources().openRawResource(R.raw.payload);
+//            inputStream = new java.io.File(R.raw.sampleaudio);
+
+        }
+        int chunkSize = 1000*1000*4;
+        byte[] bytes = new byte[chunkSize];
+        int size = 0;
+        while ((size = inputStream.read(bytes)) != -1){
+          FileUploadRequest uploadRequest = FileUploadRequest.newBuilder()
+                  .setFile(File.newBuilder().setContent(ByteString.copyFrom(bytes, 0 , size)).build())
+                  .build();
+          streamObserver.onNext(uploadRequest);
+        }
+
+// close the stream
+        inputStream.close();
+        streamObserver.onCompleted();
+        return "Complete";
+      } catch (Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        pw.flush();
+        return String.format("Failed... : %n%s", sw);
+      }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+      try {
+        channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+      Activity activity = activityReference.get();
+      if (activity == null) {
+        return;
+      }
+      TextView resultText = (TextView) activity.findViewById(R.id.grpc_response_text);
+      Button sendButton = (Button) activity.findViewById(R.id.connect_button);
+      resultText.setText(result);
+      sendButton.setEnabled(true);
+    }
+  }
+
+  public void connectTransport(WifiDirectManager wifiDirectManager){
+    String SJSUHostDeviceName = this.getString(R.string.tansport_host);
+    // we need to check and request for necessary permissions
+    if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+      requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+              HelloworldActivity.PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION);
+    }
+    if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+      requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+              HelloworldActivity.WRITE_EXTERNAL_STORAGE);
+    }
+    CompletableFuture<Boolean> completedFuture = wifiDirectManager.discoverPeers();
+    completedFuture.thenApply((b) -> {
+      Toast.makeText(HelloworldActivity.this, "Did DiscoverPeers succeed?: " + b, Toast.LENGTH_SHORT).show();
+      if( b ){
+        ArrayList<WifiP2pDevice> devices = wifiDirectManager.getPeerList();
+        Log.d(TAG, "Logging Devices: \n");
+        if(devices.isEmpty()) {
+          Log.d(TAG,"No devices found yet");
+        }
+        for(WifiP2pDevice d: devices) {
+          Log.d(TAG, d.toString());
+          if(d.deviceName.contains(SJSUHostDeviceName))
+            Log.d(TAG,"Trying to make connection with "+d.toString());
+          wifiDirectManager.connect(wifiDirectManager.makeConfig(
+                  d, false));
+        }
+      }
+      return b;
+    });
+    String message = "I tried to find some peers!: ";
+    Log.d(TAG, message);
+  }
+
+}
---
+++ BundleClient/app/src/main/java/com/ddd/wifidirect/WifiDirectBroadcastReceiver.java
@@ -0,0 +1,99 @@
+package com.ddd.wifidirect;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
+
+/**
+ * A BroadcastReceiver that notifies of important wifi p2p events.
+ * Acts as a event dispatcher to DeviceDetailFragment
+ * Whenever a WifiDirect intent event is triggered this is where
+ * we will handle the event
+ */
+public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
+
+    private WifiDirectManager manager;
+    public static final String TAG = "wDebug";
+
+    /**
+     * ctor
+     * @param manager WifiDirectManager
+     */
+    public WifiDirectBroadcastReceiver(WifiDirectManager manager) {
+        super();
+        this.manager = manager;
+    }
+
+    /**
+     * Listener callback whenever one of the registered WifiDirect Intents
+     * that were registered WifiDirectManager are triggered
+     * @param context Context/MainActivity where the intent is triggered
+     * @param intent Intent object containing triggered action.
+     */
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        // Broadcast intent action to indicate whether Wi-Fi p2p is enabled or disabled
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+
+            // Check if WifiDirect on this device is turned on.
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                Log.d(TAG, "P2P state changed Wifi Direct is enabled - " + state);
+            } else {
+                Log.d(TAG, "P2P state changed Wifi Direct is not enabled - " + state);
+            }
+
+        }
+        // Broadcast intent action indicating that the available peer list has changed.
+        // This can be sent as a result of peers being found, lost or updated.
+        else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            Log.d(TAG, "WifiDirectBroadcastReceiver INTENT PEERS_CHANGED");
+            if (manager != null) {
+                Log.d(TAG, "Requesting peers...");
+                manager.getManager().requestPeers(manager.getChannel(), this.manager);
+            }
+
+        }
+//         Broadcast intent action indicating that the state of Wi-Fi p2p
+//         connectivity has changed.
+//         EXTRA_WIFI_P2P_INFO provides the p2p connection info in the form of a WifiP2pInfo object.
+//         Another extra EXTRA_NETWORK_INFO provides the network info in the form of a NetworkInfo.
+//         A third extra provides the details of the EXTRA_WIFI_P2P_GROUP and may contain a null
+        else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            Log.d(TAG, "WifiDirect group connection CHANGED!");
+            if (manager == null) {
+                return;
+            }
+
+            // Gets the wifiP2pGroup
+            // Not needed at this time
+            // WifiP2pGroup group = (WifiP2pGroup)  intent.
+                    //getParcelableExtra(WifiP2pManager.EXTRA_WIFI_P2P_GROUP);
+
+            NetworkInfo networkInfo = (NetworkInfo) intent
+                    .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+            if (networkInfo.isConnected()) {
+                // we are connected, request connection
+                // info to find group owner IP
+                this.manager.setConnected(true);
+               this.manager.getManager().requestConnectionInfo(this.manager.getChannel(), this.manager);
+            }
+        else {
+                // It's a disconnect
+                this.manager.setConnected(false);
+                Log.d(this.manager.TAG,
+                        "WIFI_P2P_CONNECTION_CHANGED_ACTION disconnected");
+            }
+        }
+        else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // unneeded
+            // was a UI update in the orginal WifiDirect example
+        }
+    }
+}
