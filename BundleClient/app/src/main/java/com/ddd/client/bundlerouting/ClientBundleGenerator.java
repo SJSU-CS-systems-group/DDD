2782058ecb2fac710a377a73556132d948230abb
akaycee <thekcanirudh@gmail.com>
2023-04-18 23:29:27 -0700

    client bundle generator

---
+++ BundleClient/app/src/main/java/com/ddd/client/bundlerouting/ClientBundleGenerator.java
@@ -0,0 +1,45 @@
+package com.ddd.client.bundlerouting;
+
+import com.ddd.client.bundlesecurity.BundleIDGenerator;
+import com.ddd.client.bundlesecurity.SecurityExceptions.BundleIDCryptographyException;
+import com.ddd.client.bundlesecurity.SecurityExceptions.IDGenerationException;
+
+public class ClientBundleGenerator {
+    ClientBundleGenerator singleGeneratorInstance = null;
+    BundleIDGenerator bundleIDGenerator           = null;
+    ClientSecurity clientSecurity                 = null;
+
+    private ClientBundleGenerator(ClientSecurity clientSecurity)
+    {
+        this.clientSecurity = clientSecurity;
+        bundleIDGenerator   = new BundleIDGenerator();
+    }
+
+    public ClientBundleGenerator getInstance(ClientSecurity clientSecurity)
+    {
+        if (singleGeneratorInstance == null) {
+            singleGeneratorInstance = new ClientBundleGenerator(clientSecurity);
+        }
+        return singleGeneratorInstance;
+    }
+
+    public String generateBundleID() throws IDGenerationException, BundleIDCryptographyException
+    {
+        String plainBundleID = bundleIDGenerator.generateBundleID(clientSecurity.getClientKeyPath(), BundleIDGenerator.UPSTREAM);
+        return clientSecurity.encryptBundleID(plainBundleID);
+    }
+
+    public int compareBundleIDs(String id1, String id2, boolean direction) throws BundleIDCryptographyException
+    {
+        String decryptedBundleID1 = clientSecurity.decryptBundleID(id1);
+        String decryptedBundleID2 = clientSecurity.decryptBundleID(id2);
+
+        return BundleIDGenerator.compareBundleIDs(decryptedBundleID1, decryptedBundleID2, direction);
+    }
+
+    public long getCounterFromBundleID(String bundleID, boolean direction) throws BundleIDCryptographyException
+    {
+        String decryptedBundleID = clientSecurity.decryptBundleID(bundleID);
+        return BundleIDGenerator.getCounterFromBundleID(decryptedBundleID, direction);
+    }
+}
