af1c6f6d3c0fde4e60bb3d5130ec011572c102de
ben reed <ben.reed@sjsu.edu>
2024-04-17 22:33:09 -0700

    add initial checking of values

--- bundleserver/src/main/java/com/ddd/server/BundleServerApplication.java
+++ bundleserver/src/main/java/com/ddd/server/BundleServerApplication.java
@@ -7,24 +7,41 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.io.File;
 
 @SpringBootApplication
 @EnableJpaRepositories("com.ddd.server")
 @EntityScan("com.ddd.server.repository.entity")
 public class BundleServerApplication {
 
-  public static void main(String[] args) {
-    var app = new SpringApplication(BundleServerApplication.class);
-    if (CommandProcessor.checkForCommand(args)) {
-      System.out.println("Disabling stuff!");
-      app.setWebApplicationType(WebApplicationType.NONE);
-      app.setBannerMode(Banner.Mode.OFF);
-      app.setLogStartupInfo(false);
+    public static void main(String[] args) {
+        class MySpringApplication extends SpringApplication {
+            MySpringApplication(Class<?>... primarySources) {
+                super(primarySources);
+            }
+
+            @Override
+            protected void refresh(ConfigurableApplicationContext applicationContext) {
+                // check for obvious configuration errors
+                var root = applicationContext.getEnvironment().getProperty("bundle-server.bundle-store-root");
+                if (root == null || !new File(root).isDirectory()) {
+                    System.err.println("bundle-server.bundle-store-root is not a directory or not set: " + root);
+                    System.exit(1);
+                }
+                super.refresh(applicationContext);
+            }
+        }
+        var app = new MySpringApplication(BundleServerApplication.class);
+        if (CommandProcessor.checkForCommand(args)) {
+            // we are doing a CLI command, so don't start up like a server
+            app.setWebApplicationType(WebApplicationType.NONE);
+            app.setBannerMode(Banner.Mode.OFF);
+            app.setLogStartupInfo(false);
+        }
+        new BundleUploader().run(args);
+        app.run(args);
     }
-    new BundleUploader().run(args);
-    app.run(args);
-  }
 }
-// Find who instantiate the DTNCommGrpc -> should have mysql object
