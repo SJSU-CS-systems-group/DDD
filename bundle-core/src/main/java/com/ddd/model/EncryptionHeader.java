f4f5a7de7c92b792d3d851ea311a4b438c493ce6
Gokul Krishna <gokris165@gmail.com>
2024-05-29 12:50:34 -0700

    Adding unified modei files

---
+++ bundle-core/src/main/java/com/ddd/model/EncryptionHeader.java
@@ -0,0 +1,46 @@
+package com.ddd.model;
+
+import java.io.File;
+
+public class EncryptionHeader {
+  private final File serverSignedPreKey;
+  private final File serverIdentityKey;
+  private final File serverRatchetKey;
+
+  public File getServerSignedPreKey() {
+    return this.serverSignedPreKey;
+  }
+
+  public File getServerIdentityKey() {
+    return this.serverIdentityKey;
+  }
+
+  public File getServerRatchetKey() {
+    return this.serverRatchetKey;
+  }
+
+  public EncryptionHeader(File serverSignedPreKey, File serverIdentityKey, File serverRatchetKey) {
+    this.serverSignedPreKey = serverSignedPreKey;
+    this.serverIdentityKey = serverIdentityKey;
+    this.serverRatchetKey = serverRatchetKey;
+  }
+
+  private File clientBaseKey;
+  private File clientIdentityKey;
+
+  public EncryptionHeader(File clientBaseKey, File clientIdentityKey) {
+      this.clientBaseKey = clientBaseKey;
+      this.clientIdentityKey = clientIdentityKey;
+      this.serverSignedPreKey = null;
+      this.serverIdentityKey = null;
+      this.serverRatchetKey = null;
+  }
+
+  public File getClientBaseKey() {
+      return clientBaseKey;
+  }
+
+  public File getClientIdentityKey() {
+      return clientIdentityKey;
+  }
+}
