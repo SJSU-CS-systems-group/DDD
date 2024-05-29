3d6f1fe1b0cce5169a8a41d1bb7eafa7c2e2cb83
Tripti Ghanghas <triptighanghas23@gmail.com>
2024-05-29 16:15:27 -0700

    added custom properties file as program argument

--- bundleserver/src/main/java/com/ddd/server/BundleServerApplication.java
+++ bundleserver/src/main/java/com/ddd/server/BundleServerApplication.java
@@ -2,16 +2,23 @@ package com.ddd.server;
 
 import com.ddd.server.commands.CommandProcessor;
 import com.ddd.server.commands.bundleuploader.BundleUploader;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Properties;
 
+@Slf4j
 @SpringBootApplication
 @EnableJpaRepositories("com.ddd.server")
 @EntityScan("com.ddd.server.repository.entity")
@@ -28,7 +35,7 @@ public class BundleServerApplication {
                 // check for obvious configuration errors
                 var root = applicationContext.getEnvironment().getProperty("bundle-server.bundle-store-root");
                 if (root == null || !new File(root).isDirectory()) {
-                    System.err.println("bundle-server.bundle-store-root is not a directory or not set: " + root);
+                    log.error("bundle-server.bundle-store-root is not a directory or not set: " + root);
                     System.exit(1);
                 }
                 super.refresh(applicationContext);
@@ -42,6 +49,27 @@ public class BundleServerApplication {
             app.setLogStartupInfo(false);
         }
         new BundleUploader().run(args);
+
+        if (args.length > 0) {
+            Resource resource = new FileSystemResource(args[0]);
+            if (resource.exists()){
+                try {
+                    Properties properties = PropertiesLoaderUtils.loadProperties(resource);
+                    app.setDefaultProperties(properties);
+                    args = Arrays.copyOfRange(args, 1, args.length);
+                } catch (Exception e){
+                    log.error("Please enter valid properties file path!");
+                    System.exit(1);
+                }
+            }else {
+                log.error("Entered properties file path does not exist!");
+                System.exit(1);
+            }
+        }else {
+            log.error("Please enter properties file path as argument!");
+            System.exit(1);
+        }
+
         app.run(args);
     }
 }
