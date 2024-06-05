2213ce0023fef1795f7033a51494cdd9a65fdc42
Gokul Krishna <gokris165@gmail.com>
2024-06-05 13:18:20 -0700

    unified RoutingExceptions.java

---
+++ bundle-core/src/main/java/com/ddd/bundlerouting/RoutingExceptions.java
@@ -0,0 +1,15 @@
+package com.ddd.bundlerouting;
+
+public class RoutingExceptions {
+    public static class ClientMetaDataFileException extends Exception {
+        public ClientMetaDataFileException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+
+    public static class TransportIdNotFoundException extends Exception {
+        public TransportIdNotFoundException(String errorMessage) {
+            super(errorMessage);
+        }
+    }
+}
