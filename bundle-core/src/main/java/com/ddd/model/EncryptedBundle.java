4c7a96b51c8cd87158c5db5d136f2ec4b5b87f4c
Gokul Krishna <gokris165@gmail.com>
2024-06-17 15:07:28 -0700

    sanity check on the dependencies

--- bundle-core/src/main/java/com/ddd/model/EncryptedBundle.java
+++ bundle-core/src/main/java/com/ddd/model/EncryptedBundle.java
@@ -3,6 +3,9 @@ package com.ddd.model;
 import java.io.File;
 
 public class EncryptedBundle {
+
+    private static final ya = "gokul was here";
+
     private File source;
 
     public EncryptedBundle(File source) {
