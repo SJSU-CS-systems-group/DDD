e52ab1f37ba3d63f6d62c9d49981e13ddf939995
Benjamin Reed <ben.reed@sjsu.edu>
2024-06-17 22:54:53 -0700

    Use SpringBoot gRPC rather than our own (#107)

--- bundleserver/src/main/java/com/ddd/server/ServiceAdapterRegistryService.java
+++ bundleserver/src/main/java/com/ddd/server/ServiceAdapterRegistryService.java
@@ -2,35 +2,29 @@ package com.ddd.server;
 
 import com.ddd.server.repository.RegisteredAppAdapterRepository;
 import com.ddd.server.repository.entity.RegisteredAppAdapter;
-import com.ddd.server.storage.MySQLConnection;
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 import net.discdd.server.ConnectionData;
 import net.discdd.server.ResponseStatus;
 import net.discdd.server.ServiceAdapterRegistryGrpc;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.INFO;
 
-@Component
+@GrpcService
 public class ServiceAdapterRegistryService extends ServiceAdapterRegistryGrpc.ServiceAdapterRegistryImplBase {
     private static final Logger logger = Logger.getLogger(ServiceAdapterRegistryService.class.getName());
 
-    @Autowired
-    private RegisteredAppAdapterRepository registeredAppAdapterRepository;
-
-    @Autowired
-    MySQLConnection mysql;
-
-    public ServiceAdapterRegistryService(MySQLConnection mysql) {
-        this.mysql = mysql;
+    ServiceAdapterRegistryService(final RegisteredAppAdapterRepository registeredAppAdapterRepository) {
+        this.registeredAppAdapterRepository = registeredAppAdapterRepository;
     }
+
+    final private RegisteredAppAdapterRepository registeredAppAdapterRepository;
 
     @Override
     public void registerAdapter(ConnectionData connectionData, StreamObserver<ResponseStatus> responseObserver) {
-        logger.log(INFO, "Testing server from Python client");
+        logger.log(INFO, String.format("Checking %s to %s", connectionData.getAppName(), connectionData.getUrl()));
         RegisteredAppAdapter newAppAdapter =
                 new RegisteredAppAdapter(connectionData.getAppName(), connectionData.getUrl());
 
