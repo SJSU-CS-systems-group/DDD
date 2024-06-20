9508ddc40ffd0d2502de2a73d64221a0973550cc
Gokul Krishna <gokris165@gmail.com>
2024-06-20 16:29:07 -0700

    removed unused functions from server BundleSecurity.java

--- bundleserver/src/main/java/com/ddd/server/bundlesecurity/BundleSecurity.java
+++ bundleserver/src/main/java/com/ddd/server/bundlesecurity/BundleSecurity.java
@@ -2,6 +2,8 @@ package com.ddd.server.bundlesecurity;
 
 import com.ddd.model.*;
 import com.ddd.utils.Constants;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.security.GeneralSecurityException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.*;
@@ -27,6 +30,10 @@ public class BundleSecurity {
 
     private final boolean encryptionEnabled = true;
 
+    private Long getRecvdBundleIdCounter(String bundleId) {
+        return Long.valueOf(bundleId.split("-")[1]);
+    }
+
     @PostConstruct
     private void init() {
         File bundleIdNextCounter = new File(BUNDLE_ID_NEXT_COUNTER);
@@ -37,6 +44,37 @@ public class BundleSecurity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public String getClientIdFromBundleId(String bundleId) {
+        String clientId = "";
+        if (bundleId.contains("-")) {
+            clientId = bundleId.split("-")[0];
+        } else {
+            clientId = bundleId.split("#")[0];
+        }
+        logger.log(INFO, "[BundleSecurity] Client id corresponding to bundle id: " + bundleId + " is " + clientId);
+        return clientId;
+    }
+
+    public String[] encrypt(String toBeEncPath, String encPath, String bundleID, String clientID) {
+        File bundleDir = new File(encPath + File.separator + bundleID);
+        bundleDir.mkdirs();
+
+        File bundleIdFile = new File(bundleDir + File.separator + Constants.BUNDLE_IDENTIFIER_FILE_NAME);
+        try {
+            FileUtils.writeLines(bundleIdFile, Arrays.asList(bundleID));
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        try {
+            FileUtils.copyFile(new File(toBeEncPath), new File(bundleDir + File.separator + bundleID + ".jar"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        logger.log(INFO, "mock encrypt implementation");
+        return null;
     }
 
     public Payload decryptPayload(UncompressedBundle uncompressedBundle) {
@@ -90,12 +128,8 @@ public class BundleSecurity {
         return this.serverSecurity.isNewerBundle(bundlePath, lastReceivedBundleID);
     }
 
-    public String getServerId() throws NoSuchAlgorithmException {
-        return serverSecurity.getServerId();
-    }
-
     public boolean bundleServerIdMatchesCurrentServer(String receivedServerId) throws NoSuchAlgorithmException {
-        return receivedServerId.equals(getServerId());
+        return receivedServerId.equals(serverSecurity.getServerId());
     }
 
 }
