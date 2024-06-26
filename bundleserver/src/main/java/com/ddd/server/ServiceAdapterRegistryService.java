18088d585f2593ef300266e9ad1c5da95928f273
Trique Nguyen <ngngoctridung123@gmail.com>
2024-06-25 20:35:30 -0700

    register app id to REGISTER_APP_IDs.txt when connect to echo

--- bundleserver/src/main/java/com/ddd/server/ServiceAdapterRegistryService.java
+++ bundleserver/src/main/java/com/ddd/server/ServiceAdapterRegistryService.java
@@ -7,13 +7,18 @@ import net.devh.boot.grpc.server.service.GrpcService;
 import net.discdd.server.ConnectionData;
 import net.discdd.server.ResponseStatus;
 import net.discdd.server.ServiceAdapterRegistryGrpc;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.io.*;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.INFO;
 
 @GrpcService
 public class ServiceAdapterRegistryService extends ServiceAdapterRegistryGrpc.ServiceAdapterRegistryImplBase {
+    @Value("${bundle-server.registered-app-ids}")
+    private String registeredAppIdsFile;
+
     private static final Logger logger = Logger.getLogger(ServiceAdapterRegistryService.class.getName());
 
     ServiceAdapterRegistryService(final RegisteredAppAdapterRepository registeredAppAdapterRepository) {
@@ -27,6 +32,13 @@ public class ServiceAdapterRegistryService extends ServiceAdapterRegistryGrpc.Se
         logger.log(INFO, String.format("Checking %s to %s", connectionData.getAppName(), connectionData.getUrl()));
         RegisteredAppAdapter newAppAdapter =
                 new RegisteredAppAdapter(connectionData.getAppName(), connectionData.getUrl());
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(registeredAppIdsFile))) {
+            bufferedWriter.write(newAppAdapter.getAppId());
+            logger.log(INFO, String.format("Registered app to %s: %s", registeredAppIdsFile, newAppAdapter.getAppId()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         registeredAppAdapterRepository.save(newAppAdapter);
 
