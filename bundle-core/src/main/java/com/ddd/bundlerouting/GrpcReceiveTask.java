337d7623030c1a70d5217e1106bd4f47eb9e5452
Tripti Ghanghas <triptighanghas23@gmail.com>
2024-07-08 09:55:54 -0700

    initial commit

---
+++ bundle-core/src/main/java/com/ddd/bundlerouting/GrpcReceiveTask.java
@@ -0,0 +1,157 @@
+package com.ddd.bundlerouting;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.*;
+import static java.util.logging.Level.SEVERE;
+
+public class GrpcReceiveTask {
+/*
+    private static final Logger logger = Logger.getLogger(GrpcReceiveTask.class.getName());
+
+    private String host, receiveDir, transportId;
+    private int port;
+    private boolean receiveBundles, statusComplete;
+    private ManagedChannel channel;
+
+    public GrpcReceiveTask(String host, int port, String transportId, String receiveDir) {
+        logger.log(INFO, "initializing grpcreceivetask...");
+        this.host = host;
+        this.port = port;
+        this.transportId = transportId;
+        this.receiveDir = receiveDir;
+    }
+
+    public Exception run() {
+        Exception thrown = null;
+        try {
+            executeTask();
+            logger.log(FINE, "Executed receive task");
+        } catch (Exception e) {
+            thrown = e;
+        }
+
+        try {
+            postExecuteTask();
+        } catch (InterruptedException e) {
+            logger.log(WARNING, "Failed to shutdown GrpcReceiveTask channel: " + e.getMessage());
+        }
+
+        return thrown;
+    }
+
+    private void executeTask() throws Exception {
+        logger.log(INFO, "executing grpcreceivetask...");
+
+        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        BundleServiceGrpc.BundleServiceStub stub = BundleServiceGrpc.newStub(channel);
+        // using an array because it is set in the anonymous class below
+        Throwable[] thrown = new Throwable[1];
+        receiveBundles = true;
+        statusComplete = true;
+
+        StreamObserver<BundleDownloadResponse> downloadObserver = new StreamObserver<BundleDownloadResponse>() {
+            FileOutputStream fileOutputStream = null;
+            OutputStream writer;
+
+            @Override
+            public void onNext(BundleDownloadResponse response) {
+                logger.log(FINE, "onNext: called with " + response.toString());
+                if (response.hasBundleList()) {
+                    logger.log(FINE, "Got list for deletion");
+                    List<String> toDelete = Arrays.asList(response.getBundleList().getBundleList().split(","));
+                    if (!toDelete.isEmpty()) {
+                        File clientDir = new File(receiveDir);
+                        for (File bundle : clientDir.listFiles()) {
+                            if (toDelete.contains(bundle.getName())) {
+                                logger.log(INFO, "Deleting file: " + bundle.getName());
+                                bundle.delete();
+                            }
+                        }
+                    } else {
+                        logger.log(INFO, "No bundles to delete");
+                    }
+                } else if (response.hasStatus()) {
+                    logger.log(INFO, "Status found: terminating loop");
+                    receiveBundles = false;
+                } else if (response.hasMetadata()) {
+                    try {
+                        logger.log(INFO, "Downloading chunk of: " + response.getMetadata().getBid());
+                        writer = FileUtils.getFilePath(response, receiveDir);
+                    } catch (IOException e) {
+                        logger.log(WARNING,
+                                "/GrpcReceiveTask.java -> executeTask() -> onNext() IOException: " + e.getMessage());
+                    }
+                } else {
+                    try {
+                        FileUtils.writeFile(writer, response.getFile().getContent());
+                    } catch (IOException e) {
+                        logger.log(WARNING,
+                                "/GrpcReceiveTask.java -> executeTask() -> onNext() IOException: " + e.getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                logger.log(SEVERE, "Error downloading file: " + t.getMessage(), t);
+                receiveBundles = false;
+                thrown[0] = t;
+                if (fileOutputStream != null) {
+                    try {
+                        fileOutputStream.close();
+                    } catch (IOException e) {
+                        logger.log(WARNING, "/GrpcReceiveTask.java -> executeTask() -> onError() IOException: " +
+                                e.getMessage());
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCompleted() {
+                if (writer != null) {
+                    FileUtils.closeFile(writer);
+                }
+
+                logger.log(INFO, "File download complete");
+                statusComplete = true;
+            }
+        };
+
+        while (receiveBundles) {
+            if (statusComplete) {
+                logger.log(INFO, "/GrpcReceiveTask.java -> executeTask() receiveBundles = " + receiveBundles);
+
+                String existingBundles = FileUtils.getFilesList(receiveDir);
+                BundleDownloadRequest request =
+                        BundleDownloadRequest.newBuilder().setTransportId(transportId).setBundleList(existingBundles)
+                                .build();
+
+                stub.downloadBundle(request, downloadObserver);
+
+                logger.log(FINE, "Receive task complete");
+                statusComplete = false;
+            }
+        }
+        logger.log(SEVERE, "Error thrown: ", thrown[0]);
+        if (thrown[0] != null) {
+            throw new Exception(thrown[0].getMessage());
+        }
+    }
+
+    private void postExecuteTask() throws InterruptedException {
+        channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+    }*/
+}
---
+++ bundle-core/src/main/java/com/ddd/bundlerouting/GrpcSendTask.java
@@ -0,0 +1,98 @@
+package com.ddd.bundlerouting;
+
+import com.google.protobuf.ByteString;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
+import static java.util.logging.Level.*;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GrpcSendTask {
+
+ /*   private static final Logger logger = Logger.getLogger(GrpcSendTask.class.getName());
+
+    private String host, serverDir, transportId;
+    private int port;
+    private ManagedChannel channel;
+
+    public GrpcSendTask(String host, int port, String transportId, String serverDir) {
+        logger.log(INFO, "initializing grpcsendtask...");
+        this.host = host;
+        this.port = port;
+        this.transportId = transportId;
+        this.serverDir = serverDir;
+    }
+
+    public Exception run() {
+        Exception thrown = null;
+        try {
+            executeTask();
+        } catch (Exception e) {
+            thrown = e;
+        }
+
+        try {
+            postExecuteTask();
+        } catch (InterruptedException e) {
+            logger.log(WARNING, "Failed to shutdown GrpcSendTask channel: " + e.getMessage());
+        }
+
+        return thrown;
+    }
+
+    private void executeTask() throws IOException {
+        logger.log(INFO, "executing grpcsendtask...");
+
+        channel = ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        BundleServiceGrpc.BundleServiceStub stub = BundleServiceGrpc.newStub(channel);
+        StreamObserver<BundleUploadRequest> streamObserver = stub.uploadBundle(new BundleUploadObserver());
+        File sendDir = new File(serverDir);
+        logger.log(FINE, "received the stream observer: " + streamObserver);
+        //get transport ID
+        if (sendDir.exists()) {
+            File[] bundles = sendDir.listFiles();
+            if (bundles != null) {
+                for (File bundle : bundles) {
+                    BundleUploadRequest metadata = BundleUploadRequest.newBuilder().setMetadata(
+                                    BundleMetaData.newBuilder().setBid(bundle.getName()).setTransportId(transportId).build())
+                            .build();
+                    streamObserver.onNext(metadata);
+
+                    // upload file as chunk
+                    logger.log(FINE, "Started file transfer");
+                    FileInputStream inputStream = new FileInputStream(bundle.getAbsolutePath());
+                    ;
+                    *//*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                        inputStream = new FileInputStream(bundle.getAbsolutePath());
+                    }
+
+                     *//*
+                    int chunkSize = 1000 * 1000 * 4;
+                    byte[] bytes = new byte[chunkSize];
+                    int size = 0;
+                    while ((size = inputStream.read(bytes)) != -1) {
+                        logger.log(FINE, "Sending chunk size: " + size);
+                        BundleUploadRequest uploadRequest = BundleUploadRequest.newBuilder().setFile(
+                                com.ddd.bundletransport.service.File.newBuilder()
+                                        .setContent(ByteString.copyFrom(bytes, 0, size)).build()).build();
+
+                        streamObserver.onNext(uploadRequest);
+                    }
+                    inputStream.close();
+                }
+            }
+        }
+
+        logger.log(INFO, "Files are sent");
+        streamObserver.onCompleted();
+    }
+
+    private void postExecuteTask() throws InterruptedException {
+        channel.shutdown().awaitTermination(1, TimeUnit.SECONDS);
+    }*/
+}
