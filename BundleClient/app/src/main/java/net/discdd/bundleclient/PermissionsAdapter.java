8440bf325ef8cf6092222615c06a00e31d125d96
Venus Dowoang <121656507+Innominate-Majesty@users.noreply.github.com>
2024-07-10 11:49:51 -0700

    Created Permission Page (#164)

---
+++ BundleClient/app/src/main/java/net/discdd/bundleclient/PermissionsAdapter.java
@@ -0,0 +1,48 @@
+package net.discdd.bundleclient;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PermissionsAdapter extends RecyclerView.Adapter<PermissionsAdapter.PermissionViewHolder> {
+
+    private final String[] permissions;
+
+    public PermissionsAdapter(String[] permissions) {
+        this.permissions = permissions;
+    }
+
+    @NonNull
+    @Override
+    public PermissionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.permissions_list, parent, false);
+        return new PermissionViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PermissionViewHolder holder, int position) {
+        String permission = permissions[position];
+        holder.permissionCaption.setText(permission);
+    }
+
+    @Override
+    public int getItemCount() {
+        return permissions.length;
+    }
+
+    public static class PermissionViewHolder extends RecyclerView.ViewHolder {
+
+        CheckBox permissionCheckbox;
+        TextView permissionCaption;
+
+        public PermissionViewHolder(@NonNull View itemView) {
+            super(itemView);
+            permissionCheckbox = itemView.findViewById(R.id.permission_checkbox);
+            permissionCaption = itemView.findViewById(R.id.permission_caption);
+        }
+    }
+}
--- BundleClient/app/src/main/java/net/discdd/client/bundlerouting/ClientBundleGenerator.java
+++ BundleClient/app/src/main/java/net/discdd/client/bundlerouting/ClientBundleGenerator.java
@@ -18,6 +18,7 @@ import java.util.logging.Logger;
 
 public class ClientBundleGenerator {
 
+
     private static final Logger logger = Logger.getLogger(ClientBundleGenerator.class.getName());
 
     static ClientBundleGenerator singleGeneratorInstance = null;
