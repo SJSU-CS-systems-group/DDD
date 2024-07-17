cf77fba3fe87171698d9801e418057c683b5ca50
Benjamin Reed <ben.reed@sjsu.edu>
2024-07-17 12:32:38 -0700

    Radically simplified ApplicationDataManager (#170)

--- bundle-core/src/main/java/net/discdd/utils/StoreADUs.java
+++ bundle-core/src/main/java/net/discdd/utils/StoreADUs.java
@@ -1,5 +1,6 @@
 package net.discdd.utils;
 
+import com.google.protobuf.MapEntry;
 import net.discdd.model.ADU;
 import net.discdd.model.Metadata;
 import com.google.gson.Gson;
@@ -16,7 +17,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import static java.util.logging.Level.*;
 import static java.util.logging.Level.INFO;
@@ -43,7 +46,7 @@ public class StoreADUs {
         return sb.toString();
     }
 
-    private Metadata getMetadata(String clientId, String appId) throws IOException {
+    public Metadata getMetadata(String clientId, String appId) throws IOException {
         Path metadataPath = getAppFolder(clientId, appId).resolve("metadata.json");
         try {
             String data = new String(Files.readAllBytes(metadataPath));
@@ -81,7 +84,7 @@ public class StoreADUs {
         }
     }
 
-    public List<ADU> getAppData(String appId, String clientId) throws IOException {
+    public List<ADU> getAppData(String clientId, String appId) throws IOException {
         List<ADU> appDataList = new ArrayList<>();
         var folder = new File(clientId, appId);
         Metadata metadata = getMetadata(clientId, appId);
@@ -89,6 +92,17 @@ public class StoreADUs {
             appDataList.add(new ADU(new File(rootFolder + "/" + folder + "/" + i + ".txt"), appId, i, 0, clientId));
         }
         return appDataList;
+    }
+
+    public record ClientApp(String clientId, String appId) {}
+    public Stream<ClientApp> getAllClientApps() {
+        return Stream.of(rootFolder.listFiles())
+                .filter(File::isDirectory)
+                .map(File::getName)
+                .flatMap(clientId -> Stream.of(new File(rootFolder, clientId).listFiles())
+                        .filter(File::isDirectory)
+                        .map(File::getName)
+                        .map(appId -> new ClientApp(clientId, appId)));
     }
 
     public List<byte[]> getAllAppData(String appId) throws IOException {
@@ -125,7 +139,7 @@ public class StoreADUs {
         metadata.lastSentMessageId = aduId;
     }
 
-    public byte[] getADU(String clientId, String appId, String aduId) throws IOException {
+    public byte[] getADU(String clientId, String appId, Long aduId) throws IOException {
         var appFolder = getAppFolder(clientId, appId);
         var adu = Files.readAllBytes(appFolder.resolve(aduId + ".txt"));
         return adu;
@@ -136,7 +150,7 @@ public class StoreADUs {
                 rootFolder.toPath().resolve(Paths.get(clientId, appId));
     }
 
-    public File getADUFile(String clientId, String appId, String aduId) throws IOException {
+    public File getADUFile(String clientId, String appId, String aduId) {
         var appFolder = getAppFolder(clientId, appId);
         return appFolder.resolve(aduId + ".txt").toFile();
     }
@@ -166,14 +180,16 @@ public class StoreADUs {
         }
         setMetadata(clientId, appId, metadata);
         var file = new File(folder, aduId + ".txt");
-        FileOutputStream oFile = new FileOutputStream(file);
-        oFile.write(data);
-        oFile.close();
+        Files.write(file.toPath(), data);
         return file;
     }
 
     public long getLastADUIdReceived(String clientId, String appId) throws IOException {
         Metadata metadata = getMetadata(clientId, appId);
         return metadata.lastReceivedMessageId;
+    }
+    public long getLastADUIdSent(String clientId, String appId) throws IOException {
+        Metadata metadata = getMetadata(clientId, appId);
+        return metadata.lastSentMessageId;
     }
 }
