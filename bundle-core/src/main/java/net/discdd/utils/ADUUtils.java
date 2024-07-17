3befb9f656856cd792eec2886af531dea88447e4
Trique Nguyen <ngngoctridung123@gmail.com>
2024-07-17 10:45:38 -0700

    comment for build.gradle in client removed buffer write method used for testing

--- bundle-core/src/main/java/net/discdd/utils/ADUUtils.java
+++ bundle-core/src/main/java/net/discdd/utils/ADUUtils.java
@@ -20,26 +20,13 @@ public class ADUUtils {
 
     private static final Logger logger = Logger.getLogger(BundleUtils.class.getName());
 
-//    public static void writeADU(ADU adu, Path targetDirectory) throws IOException {
-//        String aduFileName = adu.getAppId() + "-" + adu.getADUId();
-//        var aduFile = targetDirectory.resolve(aduFileName + ".adu").toFile();
-//        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(
-//                adu.getSource())); BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
-//                new FileOutputStream(aduFile))) {
-//            bufferedInputStream.transferTo(bufferedOutputStream);
-//        }
-//    }
-
     public static void writeADU(ADU adu, Path targetDirectory) throws IOException {
         String aduFileName = adu.getAppId() + "-" + adu.getADUId();
         var aduFile = targetDirectory.resolve(aduFileName + ".adu").toFile();
-        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(adu.getSource()));
-             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(aduFile))) {
-            byte[] buffer = new byte[8192];
-            int bytesRead;
-            while ((bytesRead = bufferedInputStream.read(buffer)) != -1) {
-                bufferedOutputStream.write(buffer, 0, bytesRead);
-            }
+        try (BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(
+                adu.getSource())); BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(
+                new FileOutputStream(aduFile))) {
+            bufferedInputStream.transferTo(bufferedOutputStream);
         }
     }
 
