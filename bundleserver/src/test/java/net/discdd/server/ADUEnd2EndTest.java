3d013410c7c7b30e425e564e41987a4599b71057
ben reed <bcr33d@gmail.com>
2024-07-18 13:46:31 -0700

    move DDDPEMEncoder

--- bundleserver/src/test/java/net/discdd/server/ADUEnd2EndTest.java
+++ bundleserver/src/test/java/net/discdd/server/ADUEnd2EndTest.java
@@ -1,5 +1,6 @@
 package net.discdd.server;
 
+import net.discdd.bundlesecurity.DDDPEMEncoder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
@@ -12,32 +13,15 @@ import org.whispersystems.libsignal.ecc.ECKeyPair;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Base64;
 
-import static net.discdd.server.ADUEnd2EndTest.DDDPEMEncoder.ECPrivateKeyType;
-import static net.discdd.server.ADUEnd2EndTest.DDDPEMEncoder.ECPublicKeyType;
+import static net.discdd.bundlesecurity.DDDPEMEncoder.ECPrivateKeyType;
+import static net.discdd.bundlesecurity.DDDPEMEncoder.ECPublicKeyType;
 
 @SpringBootTest
 public class ADUEnd2EndTest {
     @TempDir
     static Path tempRootDir;
 
-    /**
-     * This class is a bit funky. It uses URL base64 encoding instead of the standard base64 encoding.
-     */
-    public class DDDPEMEncoder {
-
-        private static final String KEY_HEADER = "-----BEGIN %s KEY-----\n";
-        private static final String KEY_FOOTER = "\n-----END %s KEY-----";
-
-        public static final String ECPrivateKeyType = "EC PRIVATE";
-        public static final String ECPublicKeyType = "EC PUBLIC";
-
-        public static String encode(byte[] bytes, String type) {
-            String base64EncodedPrivateKey = Base64.getUrlEncoder().encodeToString(bytes);
-            return String.format(KEY_HEADER, type) + base64EncodedPrivateKey + String.format(KEY_FOOTER, type);
-        }
-    }
     @BeforeAll
     static void setup() throws IOException {
         System.setProperty("bundle-server.bundle-store-root", tempRootDir.toString() + '/');
@@ -49,7 +33,6 @@ public class ADUEnd2EndTest {
                 new IdentityKeyPair(new IdentityKey(keyPair.getPublicKey()), keyPair.getPrivateKey());
         ECKeyPair serverSignedPreKey = Curve.generateKeyPair();
         ECKeyPair serverRatchetKey = Curve.generateKeyPair();
-        System.out.println("****** Server Identity Public Key: " + keysDir.resolve("server_identity.pub"));
         Files.writeString(keysDir.resolve("serverIdentity.pub"), DDDPEMEncoder.encode(serverIdentity.getPublicKey().serialize(), ECPublicKeyType));
         Files.writeString(keysDir.resolve("serverIdentity.pvt"), DDDPEMEncoder.encode(serverIdentity.serialize(), ECPrivateKeyType));
         Files.writeString(keysDir.resolve("server_signed_pre.pub"), DDDPEMEncoder.encode(serverSignedPreKey.getPublicKey().serialize(), ECPublicKeyType));
