a5fac029e6943013613b406bbf202cd359646357
Benjamin Reed <ben.reed@sjsu.edu>
2024-07-23 14:06:19 -0700

    Refactored the naming of the ServiceAdapters (#205)

--- apps/echo/server/src/test/java/net/discdd/app/echo/EchoApplicationTests.java
+++ apps/echo/server/src/test/java/net/discdd/app/echo/EchoApplicationTests.java
@@ -1,7 +1,10 @@
 package net.discdd.app.echo;
 
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannelBuilder;
-import net.discdd.server.ServiceAdapterGrpc;
+import net.discdd.grpc.AppDataUnit;
+import net.discdd.grpc.ExchangeADUsRequest;
+import net.discdd.grpc.ServiceAdapterServiceGrpc;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,16 +18,13 @@ class EchoApplicationTests {
 
     @Test
     void echoDDDAdapter() {
-        ServiceAdapterGrpc.ServiceAdapterBlockingStub stub = ServiceAdapterGrpc.newBlockingStub(
+        ServiceAdapterServiceGrpc.ServiceAdapterServiceBlockingStub stub = ServiceAdapterServiceGrpc.newBlockingStub(
                 ManagedChannelBuilder.forAddress("localhost", 9090).usePlaintext().build());
-        var prepRsp = stub.prepareData(net.discdd.server.ClientData.newBuilder().setClientId("ben").build());
-        Assertions.assertEquals(net.discdd.server.StatusCode.SUCCESS, prepRsp.getCode());
-        var saveRsp = stub.saveData(net.discdd.server.AppData.newBuilder().setClientId("ben").addDataList(
-                net.discdd.server.AppDataUnit.newBuilder().setAduId(1)
-                        .setData(com.google.protobuf.ByteString.copyFromUtf8("hello")).build()).build());
+        var saveRsp = stub.exchangeADUs(ExchangeADUsRequest.newBuilder().setClientId("ben").addAdus(
+                AppDataUnit.newBuilder().setAduId(1).setData(ByteString.copyFromUtf8("hello")).build()).build());
         Assertions.assertEquals(1, saveRsp.getLastADUIdReceived());
-        Assertions.assertEquals(1, saveRsp.getDataListCount());
-        Assertions.assertEquals(1, saveRsp.getDataList(0).getAduId());
-        Assertions.assertEquals("hello was received", saveRsp.getDataList(0).getData().toStringUtf8());
+        Assertions.assertEquals(1, saveRsp.getAdusCount());
+        Assertions.assertEquals(1, saveRsp.getAdus(0).getAduId());
+        Assertions.assertEquals("hello was received", saveRsp.getAdus(0).getData().toStringUtf8());
     }
 }
