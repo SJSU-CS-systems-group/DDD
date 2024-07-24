f43f1dd7926da8c0dc68ec64c85932b1a3a6c829
ben reed <bcr33d@gmail.com>
2024-07-24 13:05:39 -0700

    Simplified StoreADUs metadata

--- bundle-core/src/main/java/net/discdd/utils/StoreADUs.java
+++ bundle-core/src/main/java/net/discdd/utils/StoreADUs.java
@@ -4,19 +4,16 @@ import com.google.gson.Gson;
 import net.discdd.model.ADU;
 import net.discdd.model.Metadata;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.logging.Level.FINE;
@@ -27,27 +24,9 @@ import static java.util.logging.Level.WARNING;
 public class StoreADUs {
     public Path rootFolder;
     private static final Logger logger = Logger.getLogger(StoreADUs.class.getName());
-    private boolean forSending;
-
-    public StoreADUs(Path rootFolder, boolean forSending) {
+    public StoreADUs(Path rootFolder) {
         logger.log(FINE, "bundlecore", "rootFolder: " + rootFolder);
         this.rootFolder = rootFolder;
-        this.forSending = forSending;
-    }
-
-    public StoreADUs(Path rootFolder) {
-        this(rootFolder, false);
-    }
-
-    public static String convertStreamToString(InputStream is) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        String line = null;
-        while ((line = reader.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        reader.close();
-        return sb.toString();
     }
 
     public Metadata getMetadata(String clientId, String appId) throws IOException {
@@ -59,7 +38,7 @@ public class StoreADUs {
             return gson.fromJson(data, Metadata.class);
         } catch (Exception e) {
             logger.log(SEVERE, "[FileStoreHelper] metadata not found at " + metadataPath + ". create a new one.");
-            Metadata metadata = new Metadata(0, 0, 0, 0);
+            Metadata metadata = new Metadata();
             setMetadata(clientId, appId, metadata);
             return metadata;
         }
@@ -83,34 +62,17 @@ public class StoreADUs {
         try {
             return getMetadata(clientId, appId);
         } catch (FileNotFoundException e) {
-            setMetadata(clientId, appId, new Metadata(0, 0, 0, 0));
+            setMetadata(clientId, appId, new Metadata());
             return getMetadata(clientId, appId);
         }
     }
 
     public List<ADU> getAppData(String clientId, String appId) throws IOException {
-        List<ADU> appDataList = new ArrayList<>();
         var folder = getAppFolder(clientId, appId);
-        Metadata metadata = getMetadata(clientId, appId);
-        for (long i = metadata.lastProcessedMessageId + 1; i <= metadata.lastReceivedMessageId; i++) {
-            File aduFile = rootFolder.resolve(folder.resolve(i + ".adu")).toFile();
-            if (!aduFile.exists()) {
-                logger.log(WARNING, "Could not find ADU " + i + " in: " + aduFile);
-                continue;
-            }
-            appDataList.add(new ADU(aduFile, appId, i, aduFile.length(), clientId));
+        try (var fileList = Files.list(folder)) {
+            return fileList.map(path -> new ADU(path.toFile(), appId, Long.parseLong(path.toFile().getName().split("\\.")[0]),
+                                        path.toFile().length(), clientId)).collect(Collectors.toList());
         }
-        return appDataList;
-    }
-
-    public List<ADU> getAllADUsToSend(String clientId, String appId) throws IOException {
-        List<ADU> appDataList = new ArrayList<>();
-        var folder = getAppFolder(clientId, appId);
-        Metadata metadata = getMetadata(clientId, appId);
-        for (long i = metadata.lastSentMessageId + 1; i <= metadata.lastAddedMessageId; i++) {
-            appDataList.add(new ADU(rootFolder.resolve(folder.resolve(i + ".adu")).toFile(), appId, i, 0, clientId));
-        }
-        return appDataList;
     }
 
     public record ClientApp(String clientId, String appId) {}
@@ -134,16 +96,15 @@ public class StoreADUs {
     }
 
     public List<byte[]> getAllAppData(String appId) throws IOException {
-        List<byte[]> dataList = new ArrayList<>();
-        Metadata metadata = getIfNotCreateMetadata(null, appId);
         var folder = rootFolder.resolve(appId);
-        for (long i = 1; i <= metadata.lastReceivedMessageId; i++) {
-            byte[] data = Files.readAllBytes(folder.resolve(i + ".adu"));
-            logger.log(FINE, "bundleclient", data.toString());
-            dataList.add(data);
-        }
-
-        return dataList;
+        return Files.list(folder).map(path -> {
+                try {
+                    return Files.readAllBytes(path);
+                } catch (IOException e) {
+                    logger.log(SEVERE, "Failed to read file " + path, e);
+                    return new byte[0];
+                }
+            }).collect(Collectors.toList());
     }
 
     public void deleteAllFilesUpTo(String clientId, String appId, long aduId) throws IOException {
@@ -161,14 +122,6 @@ public class StoreADUs {
         var metadata = getMetadata(clientId, appId);
         if (metadata.lastAduDeleted < aduId) {
             metadata.lastAduDeleted = aduId;
-            setMetadata(clientId, appId, metadata);
-        }
-
-        if (forSending && metadata.lastSentMessageId < aduId) {
-            metadata.lastSentMessageId = aduId;
-            setMetadata(clientId, appId, metadata);
-        } else if (!forSending && metadata.lastProcessedMessageId < aduId) {
-            metadata.lastProcessedMessageId = aduId;
             setMetadata(clientId, appId, metadata);
         }
     }
@@ -190,7 +143,7 @@ public class StoreADUs {
 
     public long getLastADUIdAdded(String clientId, String appId) throws IOException {
         Metadata metadata = getMetadata(clientId, appId);
-        return metadata.lastAddedMessageId;
+        return metadata.lastAduAdded;
     }
 
     public long getLastADUIdDeleted(String clientId, String appId) throws IOException {
@@ -218,12 +171,6 @@ public class StoreADUs {
             return null;
         }
 
-        if (forSending) {
-            metadata.lastAddedMessageId = aduId;
-        } else {
-            metadata.lastReceivedMessageId = aduId;
-        }
-
         if (metadata.lastAduAdded < lastAduId) {
             metadata.lastAduAdded = aduId;
         }
@@ -235,20 +182,5 @@ public class StoreADUs {
         oFile.close();
 
         return file;
-    }
-
-    public long getLastADUIdReceived(String clientId, String appId) throws IOException {
-        Metadata metadata = getMetadata(clientId, appId);
-        return metadata.lastReceivedMessageId;
-    }
-
-    public long getLastADUIdSent(String clientId, String appId) throws IOException {
-        Metadata metadata = getMetadata(clientId, appId);
-        return metadata.lastSentMessageId;
-    }
-
-    public long getLastADUIdProcessed(String clientId, String appId) throws IOException {
-        Metadata metadata = getMetadata(clientId, appId);
-        return metadata.lastProcessedMessageId;
     }
 }
