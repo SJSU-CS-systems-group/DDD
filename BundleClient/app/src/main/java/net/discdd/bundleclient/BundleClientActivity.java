e149ab856d66107c821fbe301490869b470330bd
Benjamin Reed <ben.reed@sjsu.edu>
2024-07-26 12:21:33 -0700

    Request needed permissions in BundleClient (#229)

--- BundleClient/app/src/main/java/net/discdd/bundleclient/BundleClientActivity.java
+++ BundleClient/app/src/main/java/net/discdd/bundleclient/BundleClientActivity.java
@@ -6,7 +6,6 @@ import static java.util.logging.Level.SEVERE;
 import static java.util.logging.Level.WARNING;
 
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -136,23 +135,6 @@ public class BundleClientActivity extends AppCompatActivity implements WifiDirec
 
     }
 
-    /**
-     * check for location permissions manually, will give a prompt
-     */
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        logger.log(INFO, "checking permissions...." + grantResults.length);
-        if (requestCode == PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION) {
-            if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                logger.log(SEVERE, "Find location is not granted....");
-                finish();
-            }
-        }
-    }
-
     @Override
     public void onReceiveAction(WifiDirectManager.WIFI_DIRECT_ACTIONS action) {
         runOnUiThread(() -> handleWifiDirectAction(action));
@@ -278,9 +260,9 @@ public class BundleClientActivity extends AppCompatActivity implements WifiDirec
 
     // ViewPagerAdapter class for managing fragments in the ViewPager
     private static class ViewPagerAdapter extends FragmentStateAdapter {
-
         private final BundleClientActivity bundleClientActivity;
         private LogFragment logFragment;
+        private PermissionsFragment permissionsFragment;
 
         public ViewPagerAdapter(@NonNull BundleClientActivity fragmentActivity) {
             super(fragmentActivity);
@@ -303,236 +285,3 @@ public class BundleClientActivity extends AppCompatActivity implements WifiDirec
         }
     }
 }
-
-//    private static final String usbDirName = "DDD_transport";
-//    public static boolean usbConnected = false;
-//    BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
-//        @Override
-//        public void onReceive(Context context, Intent intent) {
-//            String action = intent.getAction();
-//            if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-//                usbExchangeButton.setEnabled(false);
-//                usbConnectionText.setText("No USB connection detected\n");
-//                usbConnectionText.setTextColor(Color.RED);
-//                usbConnected = false;
-//                showUsbDetachedToast();
-//            } else if (UsbManager.ACTION_USB_DEVICE_ATTACHED.equals(action)) {
-//                if (usbDirExists()) {
-//                    usbExchangeButton.setEnabled(true);
-//                    usbConnectionText.setText("USB connection detected\n");
-//                    usbConnectionText.setTextColor(Color.GREEN);
-//                    usbConnected = true;
-//                    showUsbAttachedToast();
-//                } else {
-//                    usbExchangeButton.setEnabled(false);
-//                    usbConnectionText.setText("USB was connected, but /DDD_transport directory was not detected\n");
-//                    usbConnectionText.setTextColor(Color.RED);
-//                    usbConnected = false;
-//                    showUsbAttachedToast();
-//                }
-//            }
-//        }
-//    };
-
-//    @Override
-//    public void onDestroy() {
-//        super.onDestroy();
-//        wifiDirectExecutor.shutdown();
-////        wifiDirectExecutor.shutdown();
-////        unregisterReceiver(mUsbReceiver);
-//    }
-
-//        connectButton.setEnabled(true);
-//        wifiDirectResponseText.setText("Starting connection...\n");
-//        logger.log(INFO, "connecting to transport");
-//        // we need to check and request for necessary permissions
-//        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            logger.log(INFO, "requesting fine location permission");
-//            requestPermissions(new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
-//                               BundleClientActivity.PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION);
-//            logger.log(INFO, "Permission granted");
-//        }
-//        if (checkSelfPermission(Manifest.permission.NEARBY_WIFI_DEVICES) != PackageManager.PERMISSION_GRANTED) {
-//            logger.log(INFO, "requesting nearby wifi devices permission");
-//            requestPermissions(new String[] { Manifest.permission.NEARBY_WIFI_DEVICES },
-//                               BundleClientActivity.PERMISSIONS_REQUEST_CODE_ACCESS_NEARBY_WIFI_DEVICES);
-//            logger.log(INFO, "Permission granted");
-//        }
-//
-//        wifiDirectExecutor.execute(wifiDirectManager);
-
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode,
-//                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//
-//        logger.log(INFO, "checking permissions" + grantResults.length);
-//        switch (requestCode) {
-//            case PERMISSIONS_REQUEST_CODE_ACCESS_FINE_LOCATION:
-//                if (grantResults.length > 0 && grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-//                    logger.log(SEVERE, "Find location is not granted!");
-//                    finish();
-//                }
-//                break;
-//        }
-//    }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        // set up view
-//        setContentView(R.layout.activity_helloworld);
-//        connectButton = findViewById(R.id.connect_button);
-//        exchangeButton = findViewById(R.id.exchange_button);
-//        usbExchangeButton = findViewById(R.id.usb_exchange_button);
-//        resultText = findViewById(R.id.grpc_response_text);
-//        connectedDevicesText = findViewById(R.id.connected_device_address);
-//        wifiDirectResponseText = findViewById(R.id.wifidirect_response_text);
-//        usbConnectionText = findViewById(R.id.usbconnection_response_text);
-//        resultText.setMovementMethod(new ScrollingMovementMethod());
-//
-//
-//        /* Set up Server Keys before initializing Security Module */
-//        try {
-//            BundleSecurity.initializeKeyPaths(ApplicationContext.getResources(),
-//                                              ApplicationContext.getApplicationInfo().dataDir);
-//        } catch (IOException e) {
-//            logger.log(SEVERE, "[SEC]: Failed to initialize Server Keys");
-//            e.printStackTrace();
-//        }
-//
-//        try {
-//            bundleTransmission =
-//                    new BundleTransmission(Paths.get(getApplicationContext().getApplicationInfo().dataDir));
-//            clientWindow = bundleTransmission.getBundleSecurity().getClientWindow();
-//            logger.log(WARNING, "{MC} - got clientwindow " + clientWindow);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        connectButton.setOnClickListener(v -> {
-//            connectTransport();
-//        });
-//
-//        exchangeButton.setOnClickListener(v -> {
-//            if (wifiDirectManager.getDevicesFound().isEmpty()) {
-//                resultText.append("Not connected to any devices\n");
-//                return;
-//            }
-//
-//            exchangeMessage();
-//        });
-//
-//        //Registers USB receiver for device attachment and detachment
-//        IntentFilter filter = new IntentFilter();
-//        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
-//        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
-//        registerReceiver(mUsbReceiver, filter);
-//        checkUsbConnection();//used to check if usb connected before app has been started.
-//    }
-
-//    @Override
-//    public void onReceiveAction(WifiDirectManager.WIFI_DIRECT_ACTIONS action) {
-//        runOnUiThread(() -> {
-//            if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_INITIALIZATION_FAILED == action) {
-//                wifiDirectResponseText.append("Manager initialization failed\n");
-//                logger.log(WARNING, "Manager initialization failed\n");
-//            } else if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_INITIALIZATION_SUCCESSFUL ==
-//            action) {
-//                logger.log(FINER, "Manager initialization successful\n");
-//                connectTransport();
-//            } else if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_DISCOVERY_SUCCESSFUL == action) {
-//                wifiDirectResponseText.append("Discovery initiation successful\n");
-//                logger.log(FINER, "Discovery initiation successful\n");
-//            } else if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_DISCOVERY_FAILED == action) {
-//                wifiDirectResponseText.append("Discovery initiation failed\n");
-//                logger.log(WARNING, "Discovery initiation failed\n");
-//                connectButton.setEnabled(true);
-//            } else if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_PEERS_CHANGED == action) {
-//                wifiDirectResponseText.append("Peers changed\n");
-//                wifiDirectResponseText.append("Peers changed\n");
-//                logger.log(WARNING, "Peers changed\n");
-//            } else if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_CONNECTION_INITIATION_FAILED ==
-//                    action) {
-//                wifiDirectResponseText.append("Device connection initiation failed\n");
-//                logger.log(WARNING, "Device connection initiation failed\n");
-//                connectButton.setEnabled(true);
-//            } else if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_CONNECTION_INITIATION_SUCCESSFUL ==
-//                    action) {
-//                wifiDirectResponseText.append("Device connection initiation successful\n");
-//                logger.log(FINER, "Device connection initiation successful\n");
-//            } else if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_FORMED_CONNECTION_SUCCESSFUL ==
-//                    action) {
-//                wifiDirectResponseText.append("Device connected to transport\n");
-//                logger.log(FINER, "Device connected to transport\n");
-//                updateConnectedDevices();
-//                connectButton.setEnabled(true);
-//                exchangeMessage();
-//            } else if (WifiDirectManager.WIFI_DIRECT_ACTIONS.WIFI_DIRECT_MANAGER_FORMED_CONNECTION_FAILED == action) {
-//                wifiDirectResponseText.append("Device failed to connect to transport\n");
-//                logger.log(WARNING, "Device failed to connect to transport\n");
-//                connectButton.setEnabled(true);
-//            }
-//        });
-//    }
-
-//    //Usb connection methods
-//
-//    /**
-//     * Method to show a toast message indicating USB device detachment
-//     */
-//    private void showUsbDetachedToast() {
-//        Toast.makeText(this, "USB device detached", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    /**
-//     * Method to show a toast message indicating USB device attachment
-//     */
-//    private void showUsbAttachedToast() {
-//        Toast.makeText(this, "USB device attached", Toast.LENGTH_SHORT).show();
-//    }
-//
-//    /**
-//     * Checks if the /DDD_transport root directory exists in the connected usb fob.
-//     */
-//    private boolean usbDirExists() {
-//        //"/mnt/media/[uuid](getSerialNumber()?)/DDD_transport
-//        return false;
-//    }
-//
-//    /**
-//     * Method checks that a USB device is connected before app starts.
-//     */
-//    private void checkUsbConnection() {
-//        UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-//        HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
-//        if (!deviceList.isEmpty()) {
-//            //resultText.append(usbDevice.getDeviceName());
-//            if (usbDirExists()) {
-//                usbConnected = true;
-//                usbExchangeButton.setEnabled(true);
-//                usbConnectionText.setText("USB connection detected\n");
-//                usbConnectionText.setTextColor(Color.GREEN);
-//                showUsbAttachedToast();
-//            } else {
-//                usbConnected = false;
-//                usbExchangeButton.setEnabled(false);
-//                usbConnectionText.setText("USB was connected, but /DDD_transport directory was not detected\n");
-//                usbConnectionText.setTextColor(Color.RED);
-//                showUsbAttachedToast();
-//            }
-//        } else {
-//            usbExchangeButton.setEnabled(false);
-//            usbConnectionText.setText("Usb device not connected\n");
-//            usbConnectionText.setTextColor(Color.RED);
-//        }
-//    }
-//}
-
-//    // Method to update connected devices text
-//    private void updateConnectedDevices() {
-//        MainPageFragment fragment = getMainPageFragment();
-//        if (fragment != null) {
-//            fragment.updateConnectedDevicesText(wifiDirectManager.getDevicesFound());
-//        }
-//    }
--- BundleClient/app/src/main/java/net/discdd/bundleclient/PermissionsAdapter.java
+++ BundleClient/app/src/main/java/net/discdd/bundleclient/PermissionsAdapter.java
@@ -5,28 +5,39 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PermissionsAdapter extends RecyclerView.Adapter<PermissionsAdapter.PermissionViewHolder> {
 
-    private final String[] permissions;
+    final String[] permissions;
+    private final PermissionsFragment permissionsFragment;
 
-    public PermissionsAdapter(String[] permissions) {
+    public PermissionsAdapter(PermissionsFragment permissionsFragment, String[] permissions) {
         this.permissions = permissions;
+        this.permissionsFragment = permissionsFragment;
     }
 
     @NonNull
     @Override
     public PermissionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.permissions_list, parent, false);
-        return new PermissionViewHolder(view);
+        var viewHolder = new PermissionViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull PermissionViewHolder holder, int position) {
         String permission = permissions[position];
-        holder.permissionCaption.setText(permission);
+        // we are going to use the string translations keyed on the permission name to get
+        // the description of the permission
+        int resid = permissionsFragment.getResources().getIdentifier(permission, "string",
+                                                                      permissionsFragment.getContext()
+                                                                              .getPackageName());
+        holder.permissionCaption.setText(resid == 0 ? permission : permissionsFragment.getString(resid));
+        permissionsFragment.checkPermission(permission, holder);
     }
 
     @Override
