8aa6421d4c845b14799e671db95a3840ab8565da
ben reed <bcr33d@gmail.com>
2024-08-06 13:01:21 -0700

    Cleaned up BundleServer CLI
    Set the Log level to WARN if doing a CLI
    Removed timestamps from logging if doing a CLI
    Cleaned up some of the warnings
    Don't try to be a gRPC server if doing a CLI

--- bundleserver/src/main/java/net/discdd/server/repository/ServerRoutingId.java
+++ bundleserver/src/main/java/net/discdd/server/repository/ServerRoutingId.java
@@ -2,12 +2,14 @@ package net.discdd.server.repository;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Embeddable
 @Getter
 @Setter
+@EqualsAndHashCode
 public class ServerRoutingId {
     @Column(name = "transportID", nullable = false, columnDefinition = "VARCHAR(256)")
     private String transportID;
