d7e8f017dd45a2f4da5b2eba3657bd30a3da98ed
Trique Nguyen <ngngoctridung123@gmail.com>
2024-09-26 12:40:16 -0700

    security changes - move server keys to android-core resourcem -> allowing transport to utilize the keys - create transport security to for key management and generation using signal EC KeyPair - rename client security files using SecurityUtils static variables

--- BundleTransport/app/src/main/java/net/discdd/bundletransport/BundleTransportActivity.java
+++ BundleTransport/app/src/main/java/net/discdd/bundletransport/BundleTransportActivity.java
@@ -1,5 +1,6 @@
 package net.discdd.bundletransport;
 
+import static java.util.logging.Level.SEVERE;
 import static java.util.logging.Level.WARNING;
 
 import android.content.ComponentName;
@@ -25,7 +26,13 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 import net.discdd.android.fragments.LogFragment;
 import net.discdd.android.fragments.PermissionsFragment;
+import net.discdd.transport.TransportSecurity;
 
+import org.whispersystems.libsignal.InvalidKeyException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -34,6 +41,7 @@ import java.util.logging.Logger;
 
 public class BundleTransportActivity extends AppCompatActivity {
     Logger logger = Logger.getLogger(BundleTransportActivity.class.getName());
+    private TransportSecurity transportSecurity;
     private TitledFragment serverUploadFragment;
     private TitledFragment transportWifiFragment;
     private TitledFragment storageFragment;
@@ -85,6 +93,16 @@ public class BundleTransportActivity extends AppCompatActivity {
         fragments.add(new TitledFragment(getString(R.string.logs), new LogFragment()));
         TabLayout tabLayout = findViewById(R.id.tabs);
         viewPager2 = findViewById(R.id.view_pager);
+
+        var resources = getApplicationContext().getResources();
+
+        try (InputStream inServerIdentity = resources.openRawResource(
+                net.discdd.android_core.R.raw.server_identity)) {
+            this.transportSecurity = new TransportSecurity(getApplicationContext().getExternalFilesDir(null).toPath(), inServerIdentity);
+        } catch (IOException | InvalidKeyException e) {
+            logger.log(SEVERE, "[SEC]: Failed to initialize Server Keys", e);
+        }
+
         viewPager2Adapter = new FragmentStateAdapter(this) {
             @NonNull
             @Override
