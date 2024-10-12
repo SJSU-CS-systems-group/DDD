16522f18a901f245bfe629af86ec4db2c69f1d90
Trique Nguyen <ngngoctridung123@gmail.com>
2024-10-11 20:44:16 -0700

    adjusted transport paths using single path variables

--- BundleTransport/app/src/main/java/net/discdd/bundletransport/TransportWifiDirectService.java
+++ BundleTransport/app/src/main/java/net/discdd/bundletransport/TransportWifiDirectService.java
@@ -1,4 +1,5 @@
 package net.discdd.bundletransport;
+
 import android.content.Context;
 import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.SEVERE;
@@ -7,7 +8,6 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ServiceInfo;
--- bundle-core/src/main/java/net/discdd/pathutils/TransportPaths.java
+++ bundle-core/src/main/java/net/discdd/pathutils/TransportPaths.java
@@ -1,38 +1,36 @@
 package net.discdd.pathutils;
 
-import lombok.Getter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.logging.Logger;
 
 import static java.util.logging.Level.SEVERE;
 
-@Getter
 public class TransportPaths{
     private static final Logger logger = Logger.getLogger(TransportPaths.class.getName());
 
-    @Getter
-    public final Path fromClient;
-    @Getter
-    public final Path fromServer;
-    @Getter
-    public final Path toClient;
-    @Getter
-    public final Path toServer;
+    private final Path clientPath;
+    private final Path serverPath;
 
     public TransportPaths(Path rootDir){
-        this.toServer = rootDir.resolve("BundleTransmission/server");
-        this.toClient = rootDir.resolve("BundleTransmission/client");
-        this.fromClient = rootDir.resolve("BundleTransmission/server");
-        this.fromServer = rootDir.resolve("BundleTransmission/client");
+        this.clientPath = rootDir.resolve("BundleTransmission/client");
+        this.serverPath = rootDir.resolve("BundleTransmission/server");
 
         try {
-            if (!Files.exists(rootDir.resolve("BundleTransmission/server")) || !Files.isDirectory(rootDir.resolve("BundleTransmission/client"))) {
-                Files.createDirectories(rootDir.resolve("BundleTransmission/client"));
-                Files.createDirectories(rootDir.resolve("BundleTransmission/server"));
+            if (!Files.exists(clientPath) || !Files.isDirectory(serverPath)) {
+                Files.createDirectories(clientPath);
+                Files.createDirectories(serverPath);
             }
         } catch (Exception e) {
             logger.log(SEVERE, "Failed to get inventory", e);
         }
-            }
+    }
+
+    public Path getToClient(){ return clientPath; }
+
+    public Path getToServer(){ return serverPath; }
+
+    public Path getFromClient(){ return serverPath; }
+
+    public Path getFromServer(){ return clientPath; }
 }
