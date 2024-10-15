1eca56fd1232d0ba6d2066c89cee8c0e2c360d19
Trique Nguyen <ngngoctridung123@gmail.com>
2024-10-15 09:38:22 -0700

    adjusted severe message in transportPahts

--- bundle-core/src/main/java/net/discdd/pathutils/TransportPaths.java
+++ bundle-core/src/main/java/net/discdd/pathutils/TransportPaths.java
@@ -3,8 +3,16 @@ package net.discdd.pathutils;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.logging.Logger;
-
 import static java.util.logging.Level.SEVERE;
+
+// BundleTransport
+//    |_ files
+//        |_ BundelSecurity (TO-DO)
+//            |_ Server_Keys - server public keys
+//            |_ Transport_Keys - transport key pairs
+//        |_ BundleTransmission
+//            |_ client - bundles to send to client + recencyBlob
+//            |_ server - bundles to send to server
 
 public class TransportPaths{
     private static final Logger logger = Logger.getLogger(TransportPaths.class.getName());
@@ -23,7 +31,7 @@ public class TransportPaths{
                 Files.createDirectories(toServerPath);
             }
         } catch (Exception e) {
-            logger.log(SEVERE, "Failed to get inventory", e);
+            logger.log(SEVERE, "Failed to create transport storage directories", e);
         }
     }
 }
