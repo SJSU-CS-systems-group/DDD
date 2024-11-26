5be3e2a10130671efcb6d24f47ddfe0bc1b108b5
Jason ⚡⚡ <121992730+jask1m@users.noreply.github.com>
2024-11-26 13:55:59 -0800

    simplify constructors

--- BundleClient/app/src/main/java/net/discdd/bundleclient/BundleClientActivity.java
+++ BundleClient/app/src/main/java/net/discdd/bundleclient/BundleClientActivity.java
@@ -97,7 +97,7 @@ public class BundleClientActivity extends AppCompatActivity {
 
         var intent = new Intent(this, BundleClientWifiDirectService.class);
         var svc = bindService(intent, connection, Context.BIND_AUTO_CREATE);
-
+        
         mUsbReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
@@ -115,11 +115,11 @@ public class BundleClientActivity extends AppCompatActivity {
         }
 
         permissionsViewModel = new ViewModelProvider(this).get(PermissionsViewModel.class);
-        permissionsFragment = new PermissionsFragment(permissionsViewModel);
-        homeFragment = new BundleClientWifiDirectFragment();
-        usbFragment = new UsbFragment();
-        serverFragment = new ServerFragment();
-        logFragment = new LogFragment();
+        permissionsFragment = PermissionsFragment.newInstance();
+        homeFragment = BundleClientWifiDirectFragment.newInstance();
+        usbFragment = UsbFragment.newInstance();
+        serverFragment = ServerFragment.newInstance();
+        logFragment = LogFragment.newInstance()
         fragmentsWithTitles.add(new FragmentWithTitle(permissionsFragment, getString(R.string.permissions_tab)));
         connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 
