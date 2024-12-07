c7e84f4fdb2b9a563e1190289a2a735fdc86a6aa
03fernanda-palacios <03fernanda.palacios@gmail.com>
2024-12-06 17:54:47 -0800

    initial files for creash report

--- BundleClient/app/src/main/java/net/discdd/bundleclient/AcraApp.java
+++ BundleClient/app/src/main/java/net/discdd/bundleclient/AcraApp.java
@@ -1,3 +1,18 @@
 package net.discdd.bundleclient;
 
-public class AcraApp {}
+import android.app.Application;
+import android.content.Context;
+
+import org.acra.ACRA;
+import org.acra.BuildConfig;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.data.StringFormat;
+
+public class AcraApp extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ACRA.init(this, new CoreConfigurationBuilder()
+                .withBuildConfigClass(BuildConfig.class).withReportFormat(StringFormat.KEY_VALUE_LIST));
+    }
+}
--- BundleClient/app/src/main/java/net/discdd/bundleclient/LocalReportSender.java
+++ BundleClient/app/src/main/java/net/discdd/bundleclient/LocalReportSender.java
@@ -1,2 +1,55 @@
-package net.discdd.bundleclient;public class LocalReportSender {
+package net.discdd.bundleclient;
+
+import android.content.Context;
+
+import com.google.auto.service.AutoService;
+
+import org.acra.config.CoreConfiguration;
+import org.acra.data.CrashReportData;
+import org.acra.sender.ReportSender;
+import org.acra.sender.ReportSenderException;
+import org.acra.sender.ReportSenderFactory;
+import org.acra.startup.Report;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Path;
+
+public class LocalReportSender implements ReportSender {
+    CoreConfiguration config;
+
+    public LocalReportSender(CoreConfiguration coreConfiguration) {
+        config = coreConfiguration;
+    }
+
+    @Override
+    public void send(Context context, CrashReportData errorContent) throws ReportSenderException {
+        Path rootDir = context.getApplicationContext().getExternalFilesDir(null).toPath();
+        Path destDir = rootDir.resolve("BundleTransmission/server");
+        if (!destDir.toFile().exists()) {
+            destDir.toFile().mkdir();
+        }
+        File logFile = new File(String.valueOf(destDir), "crash_report.txt");
+        try {
+            String reportText = config.getReportFormat()
+                    .toFormattedString(errorContent, config.getReportContent(), "\n", "\n\t", false);
+            FileWriter writer = new FileWriter(logFile, false);
+            writer.append(reportText);
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AutoService(ReportSenderFactory.class)
+    public static class MySenderFactory implements ReportSenderFactory {
+        @NotNull
+        @Override
+        public ReportSender create(@NotNull Context context, @NotNull CoreConfiguration coreConfiguration) {
+            return new LocalReportSender(coreConfiguration);
+        }
+    }
 }
