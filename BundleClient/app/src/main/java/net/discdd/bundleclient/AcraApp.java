89e68ea0562959002441491de88b7dd2fdf4705f
03fernanda-palacios <107984309+03fernanda-palacios@users.noreply.github.com>
2025-01-28 15:34:18 -0800

    Crash report for client (#385)

---
+++ BundleClient/app/src/main/java/net/discdd/bundleclient/AcraApp.java
@@ -0,0 +1,23 @@
+package net.discdd.bundleclient;
+
+import static org.acra.ACRA.log;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.acra.ACRA;
+import org.acra.BuildConfig;
+import org.acra.config.CoreConfigurationBuilder;
+import org.acra.data.StringFormat;
+
+public class AcraApp extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        ACRA.DEV_LOGGING = true;
+        log.e("ACRA Enabled dev logging", "ACRA OK");
+
+        ACRA.init(this, new CoreConfigurationBuilder()
+                .withBuildConfigClass(BuildConfig.class).withReportFormat(StringFormat.KEY_VALUE_LIST));
+    }
+}
