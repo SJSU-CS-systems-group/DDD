c1116978e57e51e55c6c2781bfe754c26a0db74a
03fernanda-palacios <03fernanda.palacios@gmail.com>
2025-01-30 14:18:49 -0800

    implements crash report optimization

--- android-core/src/main/java/net/discdd/crashreports/LocalReportSender.java
+++ android-core/src/main/java/net/discdd/crashreports/LocalReportSender.java
@@ -11,8 +11,13 @@ import org.acra.sender.ReportSenderException;
 import org.acra.sender.ReportSenderFactory;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Path;
 
 public class LocalReportSender implements ReportSender {
@@ -24,31 +29,67 @@ public class LocalReportSender implements ReportSender {
 
     @Override
     public void send(Context context, CrashReportData errorContent) throws ReportSenderException {
-        // Iterate over the CrashReportData instance and do whatever
-        // you need with each pair of ReportField key / String value
-
-        //declare root dir and append target dir (toServer)
         Path rootDir = context.getApplicationContext().getExternalFilesDir(null).toPath();
         Path destDir = rootDir.resolve("BundleTransmission/server");
-        if (destDir.toFile().exists()) { // if a to-server path exists, we are dealing with transport
-            log.i("ACRA local reports sender ", "We are dealing with transport");
+        if (destDir.toFile().exists()) {
+            log.i("ACRA local reports sender ", "We are writing crash report to transport device storage");
             rootDir = destDir;
         } else {
-            log.i("ACRA local reports sender ", "We are dealing with client");
+            log.i("ACRA local reports sender ", "We are writing crash report to client device storage");
         }
-        File logFile = new File(String.valueOf(rootDir), "crash_report.txt"); //append crash report number or time-stamp
+        File logFile = new File(String.valueOf(rootDir), "crash_report.txt");
         try {
             // Use the core ReportFormat configuration
             String reportText = config.getReportFormat()
                     .toFormattedString(errorContent, config.getReportContent(), "\n", "\n\t", false);
 
             // Overwrite last report
+            optimizeReports(logFile);
             FileWriter writer = new FileWriter(logFile, false);
             writer.append(reportText);
             writer.flush();
             writer.close();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Does something when we have reported over five crashes
+     */
+    public void optimizeReports (File logFile) throws IOException {
+        String reportsFooterTag, lastLineToRemove;
+        reportsFooterTag = lastLineToRemove = "SHARED_PREFERENCES=default=empty";
+        int reportAmount = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(logFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.equals(reportsFooterTag)) {
+                    reportAmount++;
+                }
+            }
+        }
+        if (reportAmount >= 5) {
+            File tempFile = new File(logFile + ".tmp");
+            try (BufferedReader br = new BufferedReader(new FileReader(logFile));
+                 BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile))) {
+                String line;
+                boolean started = false;
+                while ((line = br.readLine()) != null) {
+                    if (started) {
+                        bw.write(line);
+                        bw.newLine();
+                    } else if (line.equals(lastLineToRemove)) {
+                        started = true;
+                    }
+                }
+            }
+            if (!logFile.delete()) {
+                throw new IOException("Could not delete original file");
+            }
+            if (!tempFile.renameTo(logFile)) {
+                throw new IOException("Could not rename temporary file");
+            }
         }
     }
 
