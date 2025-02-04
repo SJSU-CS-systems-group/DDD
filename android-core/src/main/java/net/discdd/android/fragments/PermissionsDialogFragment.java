a0ed0df1b8a1343ffd3bbe289a31977fc8c3ee66
jask1m <jkim94960@gmail.com>
2025-02-04 15:46:51 -0800

    dialog component implementation

--- android-core/src/main/java/net/discdd/android/fragments/PermissionsDialogFragment.java
+++ android-core/src/main/java/net/discdd/android/fragments/PermissionsDialogFragment.java
@@ -1,2 +1,23 @@
-package net.discdd.android.fragments;public class PermissionDialogFragment {
+package net.discdd.android.fragments;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+import net.discdd.android_core.R;
+
+public class PermissionsDialogFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_message)
+                .setNeutralButton(R.string.dialog_btn_text,
+                                  (dialog, id) -> System.out.println("dialog clicked."));
+        return builder.create();
+    }
+
 }
