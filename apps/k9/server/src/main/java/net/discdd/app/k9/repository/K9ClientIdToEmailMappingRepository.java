d48ce1dea19a0bd3db4da47ea0833b5880ea4859
ManasC478 <manas.chougule@gmail.com>
2025-02-11 16:01:35 -0800

    feat: create new k9 email-clientId table

--- apps/k9/server/src/main/java/net/discdd/app/k9/repository/K9ClientIdToEmailMappingRepository.java
+++ apps/k9/server/src/main/java/net/discdd/app/k9/repository/K9ClientIdToEmailMappingRepository.java
@@ -2,7 +2,12 @@ package net.discdd.app.k9.repository;
 
 import net.discdd.app.k9.repository.entity.K9ClientIdToEmailMapping;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface K9ClientIdToEmailMappingRepository  extends CrudRepository<K9ClientIdToEmailMapping, String>  {}
+public interface K9ClientIdToEmailMappingRepository  extends CrudRepository<K9ClientIdToEmailMapping, String>  {
+    @Query(value = "UPDATE k9client_id_to_email_mapping SET client_id = :newClientId WHERE email = :email", nativeQuery = true)
+    void updateClientId(@Param("newClientId") String newClientId, @Param("email") String email);
+}
--- apps/k9/server/src/main/java/net/discdd/app/k9/repository/entity/K9ClientIdToEmailMapping.java
+++ apps/k9/server/src/main/java/net/discdd/app/k9/repository/entity/K9ClientIdToEmailMapping.java
@@ -14,6 +14,38 @@ public class K9ClientIdToEmailMapping {
     public String email;
     @Column(nullable = false)
     public String clientId;
-    @Column(columnDefinition = "VARBINARY(255)", nullable = false)
-    public byte[] password;
+    @Column(columnDefinition = "text", nullable = false)
+    public String password;
+
+    public K9ClientIdToEmailMapping() {}
+
+    public K9ClientIdToEmailMapping(String email, String clientId, String password) {
+        this.email = email;
+        this.clientId = clientId;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
