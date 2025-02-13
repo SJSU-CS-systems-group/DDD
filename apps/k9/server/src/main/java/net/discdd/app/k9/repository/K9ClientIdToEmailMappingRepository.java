a5cec37d872d22a200c796a7d4bbb40c0d0c1062
ManasC478 <manas.chougule@gmail.com>
2025-02-13 14:31:08 -0800

    fix: add proper annotations

--- apps/k9/server/src/main/java/net/discdd/app/k9/repository/K9ClientIdToEmailMappingRepository.java
+++ apps/k9/server/src/main/java/net/discdd/app/k9/repository/K9ClientIdToEmailMappingRepository.java
@@ -3,11 +3,16 @@ package net.discdd.app.k9.repository;
 import net.discdd.app.k9.repository.entity.K9ClientIdToEmailMapping;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface K9ClientIdToEmailMappingRepository  extends CrudRepository<K9ClientIdToEmailMapping, String>  {
+    @Transactional
+    @Modifying
     @Query(value = "UPDATE k9client_id_to_email_mapping SET client_id = :newClientId WHERE email = :email", nativeQuery = true)
     void updateClientId(@Param("newClientId") String newClientId, @Param("email") String email);
 }
