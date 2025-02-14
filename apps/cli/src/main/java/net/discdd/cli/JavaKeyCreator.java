679ab16bccb8896dde5a6f5dc4b2ddb57089bb34
Trique Nguyen <ngngoctridung123@gmail.com>
2025-02-14 14:24:10 -0800

    cli tools for creating cert and java key pair

---
+++ apps/cli/src/main/java/net/discdd/cli/JavaKeyCreator.java
@@ -0,0 +1,27 @@
+package net.discdd.cli;
+
+import net.discdd.tls.DDDTLSUtil;
+import picocli.CommandLine;
+
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "create-java-key", description = "Create a java key pair")
+
+public class JavaKeyCreator implements Callable<Void> {
+    @CommandLine.Option(names = "--pub-out", required = true, description = "Public key file path")
+    private Path publicKeyPath;
+    @CommandLine.Option(names = "--pvt-out", required = true, description = "Private key file path")
+    private Path privateKeyPath;
+    @Override
+    public Void call() {
+        // Create a java key pair
+        try {
+            var keyPair = DDDTLSUtil.generateKeyPair();
+            DDDTLSUtil.writeKeyPairToFile(keyPair, publicKeyPath, privateKeyPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
