e93978c68ffca081f1981a2540f868677e5882c8
Trique Nguyen <ngngoctridung123@gmail.com>
2025-02-14 16:26:21 -0800

    adapter tls enabled

--- apps/echo/server/src/test/java/net/discdd/app/echo/EchoApplicationTests.java
+++ apps/echo/server/src/test/java/net/discdd/app/echo/EchoApplicationTests.java
@@ -12,6 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -26,7 +28,20 @@ class EchoApplicationTests {
     private int port;
 
     @TempDir
-    Path tempDir;
+    static Path tempDir;
+
+//    @Autowired
+//    ConfigurableApplicationContext configurableApplicationContext;
+//
+//    @BeforeEach
+//    void setUp() {
+//        TestPropertyValues.of("echo-server.rootdir=" + tempDir).applyTo(configurableApplicationContext.getEnvironment());
+//    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("echo-server.rootdir", () -> tempDir.toString());
+    }
 
     @Test
     void contextLoads() {
