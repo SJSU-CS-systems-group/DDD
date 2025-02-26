ccc7966fd6a0f5715dd594bc36651785245b6b7f
Trique Nguyen <ngngoctridung123@gmail.com>
2025-02-25 16:01:22 -0800

    adjust k9 adapter to take custom properties

--- apps/k9/server/src/main/java/net/discdd/app/k9/K9Application.java
+++ apps/k9/server/src/main/java/net/discdd/app/k9/K9Application.java
@@ -7,6 +7,10 @@ import net.discdd.grpc.ServiceAdapterRegistryServiceGrpc;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 
 import java.util.Arrays;
 import java.util.logging.Logger;
@@ -25,30 +29,44 @@ import static java.util.logging.Level.WARNING;
 public class K9Application {
     final static Logger logger = Logger.getLogger(K9Application.class.getName());
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         if (args.length < 1) {
             System.out.println("Usage: java -jar jar_file_path.jar BundleServerURL");
             System.exit(1);
         }
 
         var app = new SpringApplication(K9Application.class);
+
+        Resource resource = new FileSystemResource(args[0]);
+        if (!resource.exists()) {
+            logger.log(SEVERE, String.format("Entered properties file path %s does not exist!", args[0]));
+            System.exit(1);
+        }
+
+        try {
+            var properties = PropertiesLoaderUtils.loadProperties(resource);
+            app.setDefaultProperties(properties);
+        } catch (Exception e) {
+            logger.log(SEVERE, "Please enter valid properties file path!");
+            System.exit(1);
+        }
+
         app.setBannerMode(Banner.Mode.OFF);
         // we need to register with the BundleServer in an application initializer so that
         // the logging will be set up correctly
         app.addInitializers((actx) -> {
-            var bundleServerURL = args[0];
             var myGrpcUrl = actx.getEnvironment().getProperty("my.grpc.url");
             var appName = actx.getEnvironment().getProperty("spring.application.name");
             if (myGrpcUrl == null) {
                 logger.log(SEVERE, "my.grpc.url is not set in application.properties");
                 System.exit(1);
             }
-            var managedChannel = ManagedChannelBuilder.forTarget(bundleServerURL).usePlaintext().build();
+            var managedChannel = ManagedChannelBuilder.forTarget(myGrpcUrl).usePlaintext().build();
             var channelState = managedChannel.getState(true);
             try {
                 // TODO: remove the false when we figure out that the connect is successful!
                 if (false && channelState != ConnectivityState.READY) {
-                    logger.log(WARNING, String.format("Could not connect to %s %s", bundleServerURL, channelState));
+                    logger.log(WARNING, String.format("Could not connect to %s %s", myGrpcUrl, channelState));
                 } else {
                     var rsp = ServiceAdapterRegistryServiceGrpc.newBlockingStub(managedChannel)
                             .checkAdapterRegistration(
@@ -58,7 +76,7 @@ public class K9Application {
                                    String.format("Could not register with BundleServer: rc = %d %s", rsp.getCode(),
                                                  rsp.getMessage()));
                     }
-                    logger.log(INFO, String.format("Registered with server at %s", bundleServerURL));
+                    logger.log(INFO, String.format("Registered with server at %s", myGrpcUrl));
                 }
             } catch (Exception e) {
                 logger.log(WARNING, "Could not register with BundleServer: " + e.getMessage());
