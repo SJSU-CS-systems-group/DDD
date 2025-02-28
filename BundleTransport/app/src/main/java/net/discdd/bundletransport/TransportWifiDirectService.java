8ee110ea19096990c98e8dec492771f81d683b2e
jask1m <jkim94960@gmail.com>
2025-02-28 14:14:49 -0800

    log fragment composable migration:

--- BundleTransport/app/src/main/java/net/discdd/bundletransport/TransportWifiDirectService.java
+++ BundleTransport/app/src/main/java/net/discdd/bundletransport/TransportWifiDirectService.java
@@ -1,7 +1,5 @@
 package net.discdd.bundletransport;
 
-import android.content.Context;
-
 import static java.util.logging.Level.INFO;
 import static java.util.logging.Level.SEVERE;
 
@@ -9,6 +7,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ServiceInfo;
@@ -21,10 +20,9 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.ServiceCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
-import net.discdd.android.fragments.LogFragment;
 import net.discdd.bundlerouting.service.BundleExchangeServiceImpl;
-import net.discdd.client.bundletransmission.BundleTransmission;
 import net.discdd.pathutils.TransportPaths;
+import net.discdd.screens.LogFragment;
 import net.discdd.wifidirect.WifiDirectManager;
 import net.discdd.wifidirect.WifiDirectStateListener;
 
