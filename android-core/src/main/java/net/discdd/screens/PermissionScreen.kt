9bf327b54cf1321183e6a8cabee4c8cd49ea4f73
jask1m <jkim94960@gmail.com>
2025-03-21 13:50:06 -0700

    permissions fragment for incremental migration

--- android-core/src/main/java/net/discdd/screens/PermissionScreen.kt
+++ android-core/src/main/java/net/discdd/screens/PermissionScreen.kt
@@ -1,6 +1,11 @@
 package net.discdd.screens
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -20,13 +25,40 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewmodel.compose.viewModel
+import net.discdd.theme.ComposableTheme
 import net.discdd.viewmodels.PermissionItemData
 import net.discdd.viewmodels.PermissionsViewModel
+
+class PermissionsFragment : Fragment() {
+    private val viewModel: PermissionsViewModel by viewModels()
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { results -> viewModel.handlePermissionResults(results) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                ComposableTheme {
+                    PermissionScreen(viewModel, activityResultLauncher)
+                }
+            }
+        }
+    }
+}
 
 @Composable
 fun PermissionScreen(
