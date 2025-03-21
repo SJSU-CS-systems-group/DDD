67a93c3eb8521a36dd67777f906095067dcb640e
alder389 <alderwang06@gmail.com>
2025-03-21 16:45:26 -0700

    Reapply "finished viewmodel and screen, deleted old files"
    This reverts commit 537d1441cd5e79dd0bf1a66cedfb99482950e83a.

--- BundleTransport/app/src/main/java/net/discdd/bundletransport/screens/ServerUploadScreen.kt
+++ BundleTransport/app/src/main/java/net/discdd/bundletransport/screens/ServerUploadScreen.kt
@@ -1,43 +1,38 @@
 package net.discdd.bundletransport.screens
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.google.apps.card.v1.Columns.Column
-import net.discdd.bundletransport.viewmodels.UploadViewModel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.ui.unit.sp
-
-
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.delay
+import net.discdd.bundletransport.viewmodels.ServerUploadViewModel
+import net.discdd.viewmodels.ConnectivityViewModel
 
 class UploadFragment: Fragment() {
     override fun onCreateView(
@@ -49,7 +44,7 @@ class UploadFragment: Fragment() {
             setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
             setContent {
                 MaterialTheme {
-                    UploadScreen()
+                    ServerUploadScreen()
                 }
             }
         }
@@ -57,17 +52,34 @@ class UploadFragment: Fragment() {
 }
 
 @Composable
-fun UploadScreen(
-    uploadViewModel: UploadViewModel = viewModel(),
+fun ServerUploadScreen(
+    uploadViewModel: ServerUploadViewModel = viewModel(),
+    connectivityViewModel: ConnectivityViewModel = viewModel()
 ) {
     val uploadState by uploadViewModel.state.collectAsState()
+    val connectivityState by connectivityViewModel.state.collectAsState()
+    var connectServerBtn by remember { mutableStateOf(false) }
+
+    LaunchedEffect(uploadState.domain, uploadState.port, connectivityState.networkConnected) {
+        val enable = uploadState.domain.isNotEmpty() && uploadState.port.isNotEmpty() && connectivityState.networkConnected
+        connectServerBtn = enable
+    }
+    LaunchedEffect(uploadState.message) {
+        if (uploadState.message != null) {
+            delay(5000)
+            uploadViewModel.clearMessage()
+        }
+    }
+
     Column (
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        FilledTonalButton(onClick = {uploadViewModel.connectServer()},
+        FilledTonalButton(
+            onClick = {uploadViewModel.connectServer()},
+            enabled = connectServerBtn,
             modifier = Modifier.fillMaxWidth()) {
             Text("Connect to Bundle Server")
         }
@@ -95,13 +107,6 @@ fun UploadScreen(
             modifier = Modifier.fillMaxWidth()) {
             Text("Restore Domain and Port")
         }
-        uploadState.message?.let { message ->
-            Text(
-                text = message,
-                color = MaterialTheme.colorScheme.error,
-                style = MaterialTheme.typography.bodyMedium
-            )
-        }
         Row (
             modifier = Modifier
                 .fillMaxWidth()
@@ -117,29 +122,35 @@ fun UploadScreen(
                 fontSize = 20.sp
             )
             Text(
-                text = "   toServer: ",
+                text = "    toServer: ",
                 fontSize = 20.sp
             )
             Text (
                 text = uploadState.serverCount,
                 fontSize = 20.sp
             )
-            Text (text = "   ")
-            FilledTonalButton(onClick = {
-                uploadViewModel.reloadCount()
-            },
-                modifier = Modifier.size(170.dp, 60.dp)) {
-                Text(
-                    text = "Reload Counts",
-                    fontSize = 15.sp
-                )
-            }
+        }
+        FilledTonalButton(onClick = {
+            uploadViewModel.reloadCount()
+        },
+            modifier = Modifier
+                .size(100.dp, 70.dp)
+                .align(Alignment.End)
+        ) {
+            Text(text = "Reload Counts",)
+        }
+        uploadState.message?.let { message ->
+            Text(
+                text = message,
+                color = MaterialTheme.colorScheme.error,
+                style = MaterialTheme.typography.bodyMedium
+            )
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun UploadPreview() {
-    UploadScreen()
+fun ServerUploadPreview() {
+    ServerUploadScreen()
 }
