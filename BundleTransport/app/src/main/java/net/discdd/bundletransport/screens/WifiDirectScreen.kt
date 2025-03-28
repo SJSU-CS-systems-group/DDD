bfcf32d125cf40bf168db25df3ef14fc5aadee54
alder <alderwang06@gmail.com>
2025-03-28 14:11:19 -0700

    separated bundletransportwifievent

--- BundleTransport/app/src/main/java/net/discdd/bundletransport/screens/WifiDirectScreen.kt
+++ BundleTransport/app/src/main/java/net/discdd/bundletransport/screens/WifiDirectScreen.kt
@@ -1,2 +1,65 @@
 package net.discdd.bundletransport.screens
 
+import android.R.attr.background
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import net.discdd.bundletransport.TransportWifiDirectService
+import net.discdd.bundletransport.viewmodels.WifiDirectViewModel
+
+@Composable
+fun WifiDirectScreen(
+    wifiViewModel: WifiDirectViewModel = viewModel(),
+    preferences: SharedPreferences = LocalContext.current.getSharedPreferences(
+        TransportWifiDirectService.WIFI_DIRECT_PREFERENCES,
+        Context.MODE_PRIVATE
+    )
+) {
+    val state by wifiViewModel.
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme,background
+    ) {
+
+        Column() {
+            Text(text = "SSID: ${}")
+            Text(text = "Password: ${}")
+            Text(text = "Address: ${}")
+            Text(text = "Connected devices: ${}")
+            Text(text = "${}")
+            Text(text = "Device Name: ${}")
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Checkbox(
+                    checked = checked,
+                )
+            }
+        }
+    }
+}
+
--- BundleTransport/app/src/main/java/net/discdd/bundletransport/viewmodels/WifiDirectViewModel.kt
+++ BundleTransport/app/src/main/java/net/discdd/bundletransport/viewmodels/WifiDirectViewModel.kt
@@ -1,9 +1,21 @@
 package net.discdd.bundletransport.viewmodels
 
 import android.app.Application
-
-
+import android.content.Context.MODE_PRIVATE
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import net.discdd.bundletransport.BundleTransportActivity
+import net.discdd.bundletransport.BundleTransportWifiEvent
+import net.discdd.bundletransport.TransportWifiDirectService
+import net.discdd.pathutils.TransportPaths
+import java.util.concurrent.CompletableFuture
+import java.util.logging.Logger
 
 data class PeerDevice(
     val deviceAddress: String,
@@ -18,7 +30,7 @@ data class WifiDirectState(
     val resultText: String = "",
     val connectedDeviceText: String = "",
     val deliveryStatus: String = "",
-    val clientId: String = "Service not running",
+    val transportID: String = "Service not running",
     val backgroundExchange: Boolean = false,
     val peers: List<PeerDevice> = emptyList(),
     val showPeerDialog: Boolean = false,
@@ -29,11 +41,57 @@ class WifiDirectViewModel(
     application: Application
 ) : AndroidViewModel(application) {
     private val context get() = getApplication<Application>()
+    private val logger = Logger.getLogger(WifiDirectViewModel::class.java.name)
+    private val intentFilter = IntentFilter()
+    private val bundleTransportWifiEvent = BundleTransportWifiEvent().apply {
+        setViewModel(this@WifiDirectViewModel)
+    }
+    private val sharedPref = context.getSharedPreferences(TransportWifiDirectService.WIFI_DIRECT_PREFERENCES,
+        MODE_PRIVATE)
+    private val _state = MutableStateFlow(WifiDirectState())
+    private var btService: TransportWifiDirectService ?= null
+    private var transportPath: TransportPaths ?= null
+    val state = _state.asStateFlow()
 
 
     init {
+        intentFilter.addAction(TransportWifiDirectService.NET_DISCDD_BUNDLETRANSPORT_WIFI_EVENT_ACTION);
+        intentFilter.addAction(TransportWifiDirectService.NET_DISCDD_BUNDLETRANSPORT_CLIENT_LOG_ACTION);
+    }
+
+    fun initialize(serviceReadyFuture: CompletableFuture<TransportWifiDirectService>) {
+        viewModelScope.launch {
+            serviceReadyFuture.thenAccept { service ->
+                btService = service
+                processDeviceInfoChange()
+                updateGroupInfo()
+            }
+        }
+        this.transportPath = transportPath
+    }
+
+    fun getActivity(): BundleTransportActivity {
+        return context as BundleTransportActivity
+    }
+
+    fun processDeviceInfoChange() {
 
     }
 
-    fun exchange
+    fun registerBroadcastReceiver() {
+        LocalBroadcastManager.getInstance(context).registerReceiver(bundleTransportWifiEvent, intentFilter)
+
+    }
+
+    fun unregisterBroadcastReceiver() {
+
+    }
+
+    fun updateGroupInfo() {
+        
+    }
+
+    fun appendToClientLog(message: String) {
+
+    }
 }
