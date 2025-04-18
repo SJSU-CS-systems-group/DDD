f8552dd26e178dee7c0664a28ed463cf5be639bf
ben reed <bcr33d@gmail.com>
2025-04-18 15:03:49 -0700

    add null check

--- android-core/src/main/java/net/discdd/AndroidAppConstants.java
+++ android-core/src/main/java/net/discdd/AndroidAppConstants.java
@@ -2,11 +2,18 @@ package net.discdd;
 
 import android.content.SharedPreferences;
 
+import java.util.logging.Logger;
+
 public class AndroidAppConstants {
+    public static final Logger logger = Logger.getLogger(AndroidAppConstants.class.getName());
     public static final String BUNDLE_SERVER_DOMAIN = "ravlykmail.com";
     public static final int BUNDLE_SERVER_PORT = 7778;
 
     public static void checkDefaultDomainPortSettings(SharedPreferences sharedPref) {
+        if (sharedPref == null) {
+            logger.severe("SharedPreferences is unexpectedly null cannot set defaults");
+            return;
+        }
         if (sharedPref.getString("domain", "").isEmpty()) {
             sharedPref.edit().putString("domain", BUNDLE_SERVER_DOMAIN).apply();
         }
