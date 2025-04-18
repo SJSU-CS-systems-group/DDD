c91698da25e77b3cdca945eb7a743c3d6f55f2e5
jask1m <jkim94960@gmail.com>
2025-04-18 15:28:57 -0700

    activity migration

---
+++ BundleTransport/app/src/main/java/net/discdd/bundletransport/TransportWifiServiceManager.kt
@@ -0,0 +1,47 @@
+package net.discdd.bundletransport
+
+import android.content.ComponentName
+import android.content.ServiceConnection
+import android.os.IBinder
+import java.util.concurrent.CompletableFuture
+import java.util.logging.Logger
+
+object TransportWifiManager {
+    private val logger = Logger.getLogger(TransportWifiManager::class.java.name)
+    private var _btService: TransportWifiDirectService? = null
+
+    private val serviceReady = CompletableFuture<TransportWifiDirectService>()
+    private lateinit var connection: ServiceConnection
+
+    fun initialize(activity: MainActivity) {
+        connection = object : ServiceConnection {
+            override fun onServiceConnected(className: ComponentName, service: IBinder) {
+                // We've bound to LocalService, cast the IBinder and get LocalService instance.
+                val binder = service as TransportWifiDirectService.TransportWifiDirectServiceBinder
+                setService(binder.service)
+                serviceReady.complete(binder.service)
+            }
+            override fun onServiceDisconnected(arg0: ComponentName) {
+                clearService()
+            }
+        }
+    }
+
+    fun setService(service: TransportWifiDirectService) {
+        logger.info("Setting transport wifi service: ${service != null}")
+        _btService = service
+    }
+
+    fun clearService() {
+        logger.info("Clearing transport wifi service")
+        _btService = null
+    }
+
+    fun getService(): TransportWifiDirectService? {
+        return _btService
+    }
+
+    fun getConnection(): ServiceConnection {
+        return connection
+    }
+}
