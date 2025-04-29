6126b8aeefb8f2b94815ef8c8e9dfd94b1d44ccc
Jason ⚡ <jkim94960@gmail.com>
2025-04-29 16:17:59 -0700

    handle user input & fix lint (#520)

--- BundleTransport/app/src/main/java/net/discdd/bundletransport/screens/ServerUploadScreen.kt
+++ BundleTransport/app/src/main/java/net/discdd/bundletransport/screens/ServerUploadScreen.kt
@@ -1,5 +1,6 @@
 package net.discdd.bundletransport.screens
 
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.FilledTonalButton
@@ -26,6 +28,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,15 +43,16 @@ import net.discdd.viewmodels.SettingsViewModel
 
 @Composable
 fun ServerUploadScreen(
-        uploadViewModel: ServerUploadViewModel = viewModel(),
-        connectivityViewModel: ConnectivityViewModel = viewModel(),
-        settingsViewModel: SettingsViewModel = viewModel(),
-        onToggle: () -> Unit,
+    uploadViewModel: ServerUploadViewModel = viewModel(),
+    connectivityViewModel: ConnectivityViewModel = viewModel(),
+    settingsViewModel: SettingsViewModel = viewModel(),
+    onToggle: () -> Unit,
 ) {
     val uploadState by uploadViewModel.state.collectAsState()
     val connectivityState by connectivityViewModel.state.collectAsState()
     val showEasterEgg by settingsViewModel.showEasterEgg.collectAsState()
     var connectServerBtn by remember { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
 
     LaunchedEffect(uploadState.domain, uploadState.port, connectivityState.networkConnected) {
         val enable =
@@ -61,99 +67,106 @@ fun ServerUploadScreen(
     }
 
     Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background,
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background,
     ) {
         Column(
-                modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .pointerInput(Unit) {
+                    detectTapGestures(onTap = {
+                        focusManager.clearFocus()
+                    })
+                },
+            verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             FilledTonalButton(
-                    onClick = { uploadViewModel.connectServer() },
-                    enabled = connectServerBtn,
-                    modifier = Modifier.fillMaxWidth()
+                onClick = { uploadViewModel.connectServer() },
+                enabled = connectServerBtn,
+                modifier = Modifier.fillMaxWidth()
             ) {
                 Text("Connect to Bundle Server")
             }
 
             if (showEasterEgg) {
                 OutlinedTextField(
-                        value = uploadState.domain,
-                        onValueChange = { uploadViewModel.onDomainChanged(it) },
-                        label = { Text("Domain Input") },
-                        modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(8.dp),
+                    value = uploadState.domain,
+                    onValueChange = { uploadViewModel.onDomainChanged(it) },
+                    label = { Text("Domain Input") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
                 )
                 OutlinedTextField(
-                        value = uploadState.port,
-                        onValueChange = { uploadViewModel.onPortChanged(it) },
-                        label = { Text("Port Input") },
-                        modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(8.dp),
+                    value = uploadState.port,
+                    onValueChange = { uploadViewModel.onPortChanged(it) },
+                    label = { Text("Port Input") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
                 )
                 FilledTonalButton(
-                        onClick = { uploadViewModel.saveDomainPort() },
-                        modifier = Modifier.fillMaxWidth()
+                    onClick = { uploadViewModel.saveDomainPort() },
+                    modifier = Modifier.fillMaxWidth()
                 ) {
                     Text("Save Domain and Port")
                 }
                 FilledTonalButton(
-                        onClick = { uploadViewModel.restoreDomainPort() },
-                        modifier = Modifier.fillMaxWidth()
+                    onClick = { uploadViewModel.restoreDomainPort() },
+                    modifier = Modifier.fillMaxWidth()
                 ) {
                     Text("Restore Domain and Port")
                 }
             }
 
             Row(
-                    modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                    verticalAlignment = Alignment.CenterVertically
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
             ) {
                 /*
                 * The "toClient" section is the designated easter egg location for BundleTransport
                 * Click this portion 7 times in <3sec in order to toggle the easter egg!
                 * */
                 EasterEgg(
-                        content = { Text(text = "toClient: ", fontSize = 20.sp) },
-                        onToggle = onToggle,
+                    content = { Text(text = "toClient: ", fontSize = 20.sp) },
+                    onToggle = onToggle,
                 )
                 Text(
-                        text = uploadState.clientCount,
-                        fontSize = 20.sp
+                    text = uploadState.clientCount,
+                    fontSize = 20.sp
                 )
                 Text(
-                        text = "    toServer: ",
-                        fontSize = 20.sp
+                    text = "    toServer: ",
+                    fontSize = 20.sp
                 )
                 Text(
-                        text = uploadState.serverCount,
-                        fontSize = 20.sp
+                    text = uploadState.serverCount,
+                    fontSize = 20.sp
                 )
                 Spacer(modifier = Modifier.weight(1f))
                 FilledTonalButton(
-                        onClick = { uploadViewModel.reloadCount() },
-                        contentPadding = PaddingValues(horizontal = 12.dp, vertical = 8.dp)
+                    onClick = { uploadViewModel.reloadCount() },
+                    contentPadding = PaddingValues(horizontal = 12.dp, vertical = 8.dp)
                 ) {
                     Icon(
-                            imageVector = Icons.Default.Refresh,
-                            contentDescription = "Reload Counts",
-                            modifier = Modifier.size(24.dp)
+                        imageVector = Icons.Default.Refresh,
+                        contentDescription = "Reload Counts",
+                        modifier = Modifier.size(24.dp)
                     )
                 }
             }
 
             uploadState.message?.let { message ->
                 Text(
-                        text = message,
-                        color = MaterialTheme.colorScheme.error,
-                        style = MaterialTheme.typography.bodyMedium
+                    text = message,
+                    color = MaterialTheme.colorScheme.error,
+                    style = MaterialTheme.typography.bodyMedium
                 )
             }
         }
