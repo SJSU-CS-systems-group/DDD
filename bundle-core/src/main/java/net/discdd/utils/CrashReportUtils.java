c9992241a288221df6cebbdf9e1366c01c33d3e7
03fernanda-palacios <03fernanda.palacios@gmail.com>
2025-05-01 15:17:19 -0700

    prev added utils file now reads and verifies crash reports existance

--- bundle-core/src/main/java/net/discdd/utils/CrashReportUtils.java
+++ bundle-core/src/main/java/net/discdd/utils/CrashReportUtils.java
@@ -1,2 +1,32 @@
-package net.discdd.utils;public class CrashReportUtils {
+package net.discdd.utils;
+
+import net.discdd.model.Acknowledgement;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CrashReportUtils {
+    private static final Logger logger = Logger.getLogger(CrashReportUtils.class.getName());
+
+    public static String readCrashReportFromFile(Path reportFilePath) {
+        try {
+            return new String(Files.readAllBytes(reportFilePath));
+        } catch (IOException e) {
+            logger.log(Level.WARNING, "Failed to read the crash report from " + reportFilePath, e);
+        }
+        return null;
+    }
+
+    public static boolean crashReportExists(String reportFilePath) {
+        File potentialReport = new File(reportFilePath);
+        if (potentialReport.exists()) {
+            return true;
+        }
+        return false;
+    }
 }
