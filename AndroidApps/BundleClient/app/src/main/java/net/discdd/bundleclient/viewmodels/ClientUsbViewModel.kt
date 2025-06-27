4fca4bfb51d97f63e9d518f222f8897f279721ec
03fernanda-palacios <03fernanda.palacios@gmail.com>
2025-06-26 17:11:23 -0700

    prevent client from creating multiple dirs if they already exist

--- AndroidApps/BundleClient/app/src/main/java/net/discdd/bundleclient/viewmodels/ClientUsbViewModel.kt
+++ AndroidApps/BundleClient/app/src/main/java/net/discdd/bundleclient/viewmodels/ClientUsbViewModel.kt
@@ -9,16 +9,30 @@ import kotlinx.coroutines.launch
 import net.discdd.bundleclient.R
 import net.discdd.bundleclient.WifiServiceManager
 import net.discdd.viewmodels.UsbViewModel
+import java.io.IOException
 import java.util.logging.Level
+import java.util.logging.Level.INFO
+import java.util.logging.Logger
 
 class ClientUsbViewModel(
     application: Application,
 ) : UsbViewModel(application) {
+    private val logger: Logger = Logger.getLogger(UsbViewModel::class.java.name)
     private val bundleTransmission by lazy {
         val wifiBgService = WifiServiceManager.getService()
         wifiBgService?.bundleTransmission
     }
 
+    fun createIfDoesNotExist(parent : DocumentFile, name : String) : DocumentFile {
+        var child = parent.findFile(name)
+        if (child?.isDirectory ?: true) {
+            child = parent.createDirectory("server")
+            logger.log(INFO, "\'$name\' directory has been created")
+        } else {
+            logger.log(INFO, "\'$name\' directory exists")
+        }
+        return child ?: throw IOException ("Could not create $name")
+    }
     fun transferBundleToUsb(context: Context) {
         val dddDir = usbDirectory
         if (!state.value.dddDirectoryExists || dddDir == null || bundleTransmission == null) {
@@ -32,12 +46,8 @@ class ClientUsbViewModel(
         val bundleDTO = bundleTransmission!!.generateBundleForTransmission()
         val bundleFile = bundleDTO.bundle.source
 
-        val dddSendDir = dddDir.createDirectory("toserver")
-        if (dddSendDir == null) {
-            updateMessage("Error creating ddd_send directory in USB directory", Color.RED)
-            return
-        }
-        val targetFile = dddSendDir.createFile("data/octet", bundleFile.name)
+        var dddServer = createIfDoesNotExist(dddDir, "toServer")
+        val targetFile = dddServer.createFile("data/octet", bundleFile.name)
         if (targetFile == null) {
             updateMessage("Error creating target file in USB directory", Color.RED)
             return
