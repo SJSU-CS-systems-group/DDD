8488b2c5481fd599da0fb8bb8286cbaaa12ada5d
Benjamin Reed <ben.reed@sjsu.edu>
2025-06-29 19:52:33 -0700

    cleanup versioning and redundancies in build (#601)
    * fixes some version conflicts we had
    * removes redundant gradle files
    * fixed okhttp/netty mixup in BundleTransmission

--- apps/cli/src/main/java/net/discdd/cli/Main.java
+++ apps/cli/src/main/java/net/discdd/cli/Main.java
@@ -3,19 +3,11 @@ package net.discdd.cli;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 
-import java.util.concurrent.Callable;
-
 @Command(name = "cli", subcommands = { DecryptBundle.class,
                                        EncryptBundle.class,
                                        SelfSignedCertCreator.class,
-                                       JavaKeyCreator.class })
-public class Main implements Callable<Integer> {
-
-    @Override
-    public Integer call() {
-        System.out.println("Subcommand needed: 'encrypt' or 'decrypt'");
-        return 0;
-    }
+                                       JavaKeyCreator.class }, mixinStandardHelpOptions = true)
+public class Main {
 
     public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format", "%5$s%6$s%n");
