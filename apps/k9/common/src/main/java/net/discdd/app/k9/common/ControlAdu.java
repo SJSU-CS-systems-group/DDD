568a10225028df4e15b28b4613ad0d4bc0331e2d
ben reed <ben.reed@sjsu.edu>
2025-07-03 10:05:43 -0700

    ControlAdu for K9 common

---
+++ apps/k9/common/src/main/java/net/discdd/app/k9/common/ControlAdu.java
@@ -0,0 +1,207 @@
+package net.discdd.app.k9.common;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.Properties;
+
+import static java.lang.String.format;
+
+/**
+ * this interface uses properties to convert bytes to and from Adu records.
+ * the records are stored as a property file that starts with a comment "# CONTROL".
+ */
+public class ControlAdu {
+    public static final Map<String, Class<? extends ControlAdu>> ADU_TYPES = Map.of("login",
+                                                                                    LoginControlAdu.class,
+                                                                                    "login-ack",
+                                                                                    LoginAckControlAdu.class,
+                                                                                    "register",
+                                                                                    RegisterControlAdu.class,
+                                                                                    "register-ack",
+                                                                                    RegisterAckControlAdu.class,
+                                                                                    "whoami",
+                                                                                    WhoAmIControlAdu.class,
+                                                                                    "whoami-ack",
+                                                                                    WhoAmIAckControlAdu.class);
+
+    // ugly hack to turn off strict method checking when creating ControlAdu instances
+    // from deserialized properties.
+    private static final ThreadLocal<Boolean> checkMethods = ThreadLocal.withInitial(() -> Boolean.TRUE);
+    // The header for control ADUs, used to identify them in the byte stream.
+    // start with a # to make it compatible with properties files
+    public static final String CONTROL_HEADER = "# CONTROL";
+    final protected Properties properties = new Properties();
+    ControlAdu(Map<String, Object> map) {
+        if (map == null || map.isEmpty()) {
+            return;
+        }
+        Class<? extends ControlAdu> clazz = this.getClass();
+        for (var entry : map.entrySet()) {
+            // make sure the method exists
+            String key = entry.getKey();
+            try {
+                var ignored = clazz.getMethod(key);
+            } catch (NoSuchMethodException e) {
+                if (checkMethods.get()) {
+                    throw new IllegalArgumentException(format("%s is not a method of %s", key, clazz.getName()), e);
+                }
+            }
+            var value = entry.getValue();
+            properties.put(key, value.toString());
+        }
+    }
+
+    public static boolean isControlAdu(byte[] bytes) {
+        if (bytes == null || bytes.length < CONTROL_HEADER.length()) {
+            return false;
+        }
+        for (int i = 0; i < CONTROL_HEADER.length(); i++) {
+            if (bytes[i] != CONTROL_HEADER.charAt(i)) {
+                return false;
+            }
+        }
+        return bytes[CONTROL_HEADER.length()] == '\n';
+    }
+
+    public static ControlAdu fromBytes(byte[] bytes) throws IOException {
+        if (!isControlAdu(bytes)) {
+            throw new IOException("Not a control ADU");
+        }
+        var props = new Properties();
+        try (var is = new ByteArrayInputStream(bytes)) {
+            props.load(is);
+        }
+        String type = props.getProperty("type");
+        if (type == null || !ADU_TYPES.containsKey(type)) {
+            throw new IOException("Unknown ADU type: " + type);
+        }
+        try {
+            checkMethods.set(false);
+            var clazz = ADU_TYPES.get(type);
+            Constructor<? extends ControlAdu> ctor = clazz.getConstructor(Map.class);
+            return ctor.newInstance(props);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException |
+                 InstantiationException e) {
+            throw new IOException("Failed to create ADU from bytes", e);
+        } finally {
+            checkMethods.set(true);
+        }
+    }
+
+    public byte[] toBytes() throws IOException {
+        String typeKey = null;
+        for (var entry : ADU_TYPES.entrySet()) {
+            if (entry.getValue().isInstance(this)) {
+                typeKey = entry.getKey();
+                break;
+            }
+        }
+        if (typeKey == null) {
+            throw new IOException("Unknown ADU type: " + this.getClass().getName());
+        }
+        properties.put("type", typeKey);
+        var baos = new ByteArrayOutputStream();
+        baos.writeBytes(CONTROL_HEADER.getBytes());
+        baos.write('\n');
+        properties.store(baos, null);
+        return baos.toByteArray();
+    }
+
+    @Override
+    public String toString() {
+        var sos = new ByteArrayOutputStream();
+        try {
+            sos.writeBytes(CONTROL_HEADER.getBytes());
+            sos.write('\n');
+            properties.store(sos, null);
+            return sos.toString();
+        } catch (IOException e) {
+            return "ControlAdu{error=" + e.getMessage() + "}";
+        }
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof ControlAdu that)) return false;
+        return properties.equals(that.properties);
+    }
+
+    @Override
+    public int hashCode() {
+        return properties.hashCode();
+    }
+
+    public static class LoginControlAdu extends ControlAdu {
+        public LoginControlAdu(Map<String, Object> properties) {
+            super(properties);
+        }
+
+        public String username() {return properties.getProperty("username");}
+
+        public String password() {return properties.getProperty("password");}
+    }
+
+    public static class LoginAckControlAdu extends ControlAdu {
+        public LoginAckControlAdu(Map<String, Object> properties) {
+            super(properties);
+        }
+
+        public String email() {return properties.getProperty("email");}
+
+        public String password() {return properties.getProperty("password");}
+
+        public boolean success() {return Boolean.parseBoolean(properties.getProperty("success"));}
+
+        public String message() {return properties.getProperty("message");}
+    }
+
+    public static class RegisterControlAdu extends ControlAdu {
+        public RegisterControlAdu(Map<String, Object> properties) {
+            super(properties);
+        }
+
+        public String prefix() {return properties.getProperty("prefix");}
+
+        public String suffix() {return properties.getProperty("suffix");}
+
+        public String password() {return properties.getProperty("password");}
+    }
+
+    public static class RegisterAckControlAdu extends ControlAdu {
+        public RegisterAckControlAdu(Map<String, Object> properties) {
+            super(properties);
+        }
+
+        public String email() {return properties.getProperty("email");}
+
+        public String password() {return properties.getProperty("password");}
+
+        public boolean success() {return Boolean.parseBoolean(properties.getProperty("success"));}
+
+        public String message() {return properties.getProperty("message");}
+    }
+
+    public static class WhoAmIControlAdu extends ControlAdu {
+        public WhoAmIControlAdu() {
+            super(null);
+        }
+        // we need this one for deserialization
+        public WhoAmIControlAdu(Map<String, Object> properties) {
+            super(properties);
+        }
+    }
+
+    public static class WhoAmIAckControlAdu extends ControlAdu {
+        public WhoAmIAckControlAdu(Map<String, Object> properties) {
+            super(properties);
+        }
+
+        public String email() {return properties.getProperty("email");}
+    }
+
+}
---
+++ apps/k9/common/src/test/java/net/discdd/app/k9/common/ControlAduTest.java
@@ -0,0 +1,43 @@
+package net.discdd.app.k9.common;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class ControlAduTest {
+    @Test
+    public void testControlAdus() throws IOException {
+        var origLoginAdu = new ControlAdu.LoginControlAdu(Map.of("username", "testuser", "password", "testpass"));
+        var serDeserLoginAdu = ControlAdu.fromBytes(origLoginAdu.toBytes());
+        Assertions.assertEquals(origLoginAdu, serDeserLoginAdu);
+
+        var origLoginAckAdu = new ControlAdu.LoginAckControlAdu(Map.of("email", "testemail", "password", "testpass", "success", "true"));
+        var serDeserLoginAckAdu = ControlAdu.fromBytes(origLoginAckAdu.toBytes());
+        Assertions.assertEquals(origLoginAckAdu, serDeserLoginAckAdu);
+
+        var origRegisterAdu = new ControlAdu.RegisterControlAdu(Map.of("prefix", "testprefix", "suffix", "testsuffix", "password", "testpass"));
+        var serDeserRegisterAdu = ControlAdu.fromBytes(origRegisterAdu.toBytes());
+        Assertions.assertEquals(origRegisterAdu, serDeserRegisterAdu);
+
+        var origRegisterAckAdu = new ControlAdu.RegisterAckControlAdu(Map.of("email", "testemail", "password", "testpass"));
+        var serDeserRegisterAckAdu = ControlAdu.fromBytes(origRegisterAckAdu.toBytes());
+        Assertions.assertEquals(origRegisterAckAdu, serDeserRegisterAckAdu);
+
+        var origWhoAmIControlAdu = new ControlAdu.WhoAmIControlAdu();
+        var serDeserWhoAmIControlAdu = ControlAdu.fromBytes(origWhoAmIControlAdu.toBytes());
+        Assertions.assertEquals(origWhoAmIControlAdu, serDeserWhoAmIControlAdu);
+
+        var origWhoAmIAckControlAdu = new ControlAdu.WhoAmIAckControlAdu(Map.of("email", "testemail"));
+        var serDeserWhoAmIAckControlAdu = ControlAdu.fromBytes(origWhoAmIAckControlAdu.toBytes());
+        Assertions.assertEquals(origWhoAmIAckControlAdu, serDeserWhoAmIAckControlAdu);
+    }
+
+    @Test
+    public void testControlAduExtraData() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new ControlAdu.LoginControlAdu(Map.of("username", "testuser", "password", "testpass", "extraData", "testdata"));
+        });
+    }
+}
