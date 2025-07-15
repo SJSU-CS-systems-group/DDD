09114b6d7d64191617a6ff22ef1e35f7a3f8a440
ben reed <ben.reed@sjsu.edu>
2025-07-15 23:42:46 +0300

    0.2-beta3 code 12 HOTFIX

--- AndroidApps/BundleTransport/app/src/main/java/net/discdd/bundletransport/screens/WifiDirectScreen.kt
+++ AndroidApps/BundleTransport/app/src/main/java/net/discdd/bundletransport/screens/WifiDirectScreen.kt
@@ -5,21 +5,12 @@ import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -32,12 +23,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.startActivity
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
@@ -47,12 +36,10 @@ import com.google.accompanist.permissions.rememberPermissionState
 import net.discdd.bundletransport.BundleTransportService
 import net.discdd.bundletransport.R
 import net.discdd.bundletransport.viewmodels.WifiDirectViewModel
-import net.discdd.components.WifiPermissionBanner
-import java.util.concurrent.CompletableFuture
-import androidx.core.content.edit
-import net.discdd.components.ScrollingColumn
 import net.discdd.components.UserLogComponent
+import net.discdd.components.WifiPermissionBanner
 import net.discdd.utils.UserLogRepository
+import java.util.concurrent.CompletableFuture
 
 @OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
@@ -74,7 +61,7 @@ fun WifiDirectScreen(
             wifiViewModel.initialize(serviceReadyFuture)
         }
 
-        ScrollingColumn(showActionButton = false, onActionClick = {}) {
+        Column {
             if (wifiViewModel.isWifiEnabled()) {
                 val nameValid by remember {
                     derivedStateOf { state.deviceName.startsWith("ddd_") }
