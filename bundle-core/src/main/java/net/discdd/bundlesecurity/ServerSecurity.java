28c8e19c1526fcdd72ee431ad220f0c62b28b324
abhip1106 <abhishekprabhu04@gmail.com>
2025-09-05 14:28:29 -0700

    Transactional Commits

--- bundle-core/src/main/java/net/discdd/bundlesecurity/ServerSecurity.java
+++ bundle-core/src/main/java/net/discdd/bundlesecurity/ServerSecurity.java
@@ -312,10 +312,9 @@ public class ServerSecurity {
 
         var encryptedData = Files.newInputStream(payloadPath.resolve(payloadName));
         var output = Files.newOutputStream(decryptedFile, StandardOpenOption.APPEND, StandardOpenOption.CREATE);
-
-        client.cipherSession.decrypt(encryptedData, output);
-
-        updateSessionRecord(client);
+        if(client.cipherSession.decrypt(encryptedData, decryptedFile)){
+            updateSessionRecord(client);
+        }
 
         logger.log(FINE, "[ServerSecurity]:Decrypted Size = %d", Files.size(decryptedFile));
 
