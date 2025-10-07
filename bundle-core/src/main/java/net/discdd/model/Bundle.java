e743d99db722c1588a531da4a23997730333a3e3
AbhishekPrabhu0914 <128254190+AbhishekPrabhu0914@users.noreply.github.com>
2025-10-07 15:38:21 -0700

    Bundle DTO removed (#676)
    Merged BundleDTO and Bundle class into one, removing BundleDTO and
    adding an id field to Bundle

--- bundle-core/src/main/java/net/discdd/model/Bundle.java
+++ bundle-core/src/main/java/net/discdd/model/Bundle.java
@@ -4,11 +4,18 @@ import java.io.File;
 
 public class Bundle {
     private final File source;
-
-    public Bundle(File source) {
+    private final String id;
+    public Bundle(String id, File source) {
         this.source = source;
+        this.id = id;
     }
-
+    public Bundle(File source){
+        this.source = source;
+        this.id = null;
+    }
+    public String getBundleId() {
+        return id;
+    }
     public File getSource() {
         return this.source;
     }
