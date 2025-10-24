db0f12cde0687b7f32920fbaf456d450b5b6b919
funsize1324 <cjlou45@icloud.com>
2025-10-24 13:48:24 -0700

    transport message repository

---
+++ bundleserver/src/main/java/net/discdd/server/repository/entity/TransportMessage.java
@@ -0,0 +1,20 @@
+package net.discdd.server.repository.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransportMessage {
+    @Id
+    public String transportId;
+
+    /**
+     * The message being sent to the transport
+     */
+    public String message;
+
+}
