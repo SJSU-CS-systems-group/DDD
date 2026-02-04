ed53f327b5839563074e96a18342805dcc20b2f2
jay <jlc.timbol@gmail.com>
2026-02-03 16:45:06 -0800

    Code cleanup: Fix redundant Map.get() call in ServerSecurity (#739)
    issue #727 
    replaced .get() w/ already-retrieved value

--- bundle-core/src/main/java/net/discdd/bundlesecurity/ServerSecurity.java
+++ bundle-core/src/main/java/net/discdd/bundlesecurity/ServerSecurity.java
@@ -228,7 +228,7 @@ public class ServerSecurity {
             IOException {
         var clientSession = clientMap.get(clientID);
         if (clientSession != null) {
-            return clientMap.get(clientID);
+            return clientSession;
         }
         var keyPath = clientRootPath.resolve(clientID);
         SessionRecord clientSessionRecord = null;
