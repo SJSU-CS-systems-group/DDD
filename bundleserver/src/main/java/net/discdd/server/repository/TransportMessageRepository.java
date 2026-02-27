877080f7e3ae7df328c3ed0b9de57a7f4f124235
funsize1324 <cjlou45@icloud.com>
2026-02-27 14:11:47 -0800

    added findMessagesAfter and implemented checkMessages

--- bundleserver/src/main/java/net/discdd/server/repository/TransportMessageRepository.java
+++ bundleserver/src/main/java/net/discdd/server/repository/TransportMessageRepository.java
@@ -7,11 +7,17 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransportMessageRepository extends CrudRepository<TransportMessage, MessageKey> {
 
     @Query("SELECT MAX(t.messageKey.messageNumber) FROM TransportMessage t WHERE t.messageKey.transportId = " +
             ":transportId")
     Long findMaxMessageNumber(@Param("transportId") String transportId);
+
+    @Query("SELECT t FROM TransportMessage t WHERE t.messageKey.transportId = :transportId AND t.messageKey.messageNumber > :lastMessageId ORDER BY t.messageKey.messageNumber ASC")
+    List<TransportMessage> findMessagesAfter(@Param("transportId") String transportId,
+                                             @Param("lastMessageId") long lastMessageId);
 
 }
