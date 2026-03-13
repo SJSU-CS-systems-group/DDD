1306cf82ec8f90a2c7e477548b1dd56a77e6d7fa
matildaVerdejo <168225905+matildaVerdejo@users.noreply.github.com>
2026-03-13 12:26:58 -0700

    Update AndroidApps/BundleClient/app/src/main/java/net/discdd/bundleclient/service/wifiDirect/DDDWifiDirect.java
    Co-authored-by: 03fernanda-palacios <107984309+03fernanda-palacios@users.noreply.github.com>

--- AndroidApps/BundleClient/app/src/main/java/net/discdd/bundleclient/service/wifiDirect/DDDWifiDirect.java
+++ AndroidApps/BundleClient/app/src/main/java/net/discdd/bundleclient/service/wifiDirect/DDDWifiDirect.java
@@ -250,7 +250,6 @@ public class DDDWifiDirect implements DDDWifi {
         wifiP2pManager.requestP2pState(wifiChannel, this::processStateChange);
         //sync discovery state at a registration time, not just from broadcasts
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {   //TO DO: delete because we always target SDK_INIT above 33
-            logger.info("Requesting current Wifi P2p discovery state");
             wifiP2pManager.requestDiscoveryState(wifiChannel, state -> {
                 discoveryActive = state == WifiP2pManager.WIFI_P2P_DISCOVERY_STARTED;
                 eventsLiveData.postValue(DDDWifiEventType.DDDWIFI_DISCOVERY_CHANGED);
