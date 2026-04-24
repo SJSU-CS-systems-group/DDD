122f20bfe6e9e3fb9a202cc83396e4857002d91c
03fernanda-palacios <03fernanda.palacios@gmail.com>
2026-04-24 11:49:29 -0700

    server to handle multiple client reports per bundle

--- bundle-core/src/main/java/net/discdd/utils/BundleUtils.java
+++ bundle-core/src/main/java/net/discdd/utils/BundleUtils.java
@@ -331,7 +331,7 @@ public class BundleUtils {
             // add the records to the inner jar
             innerJar.createEntry("acknowledgement.txt", ackedEncryptedBundleId.getBytes());
             innerJar.createEntry("routing.metadata", routingData == null ? "{}".getBytes() : routingData);
-            for (var report: crashReports) {
+            for (var report: crashReports == null ? List.<Path>of() : crashReports) {
                 try (var os = innerJar.createEntry(Paths.get(Constants.BUNDLE_CRASH_REPORT_DIRECTORY_NAME,
                                                              report.getFileName().toString()));
                      var aos = Files.newInputStream(report, StandardOpenOption.READ)) {
