bcceb557b8cd520b203a12591c71eaf5e5cfc36f
03fernanda-palacios <03fernanda.palacios@gmail.com>
2026-04-28 14:02:51 -0700

    re-adds return

--- AndroidApps/android-core/src/main/java/net/discdd/crashreports/LocalReportSender.java
+++ AndroidApps/android-core/src/main/java/net/discdd/crashreports/LocalReportSender.java
@@ -47,6 +47,7 @@ public class LocalReportSender implements ReportSender {
             currIndex = optimizeReports(toBeBundledDir);
         } catch (IOException e) {
             logger.log(SEVERE, "Optimizing reports on this device failed" + e);
+            return;
         }
         File logFile = new File(String.valueOf(toBeBundledDir), "crash_report" + currIndex + ".txt");
         try {
