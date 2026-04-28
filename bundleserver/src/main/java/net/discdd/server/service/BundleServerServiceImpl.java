654da63c126f418744c2d4c0dc073185230fb31c
03fernanda-palacios <03fernanda.palacios@gmail.com>
2026-04-28 13:48:20 -0700

    resolves non-conflict conflict

--- bundleserver/src/main/java/net/discdd/server/service/BundleServerServiceImpl.java
+++ bundleserver/src/main/java/net/discdd/server/service/BundleServerServiceImpl.java
@@ -136,16 +136,16 @@ public class BundleServerServiceImpl extends BundleServerServiceGrpc.BundleServe
 
     @Override
     public void crashReports(CrashReportRequest request, StreamObserver<CrashReportResponse> response) {
+        //var data = request.getCrashReportData();
         X509Certificate clientCert = NettyServerCertificateInterceptor.CLIENT_CERTIFICATE_KEY.get(Context.current());
         var name = DDDTLSUtil.publicKeyToName(clientCert.getPublicKey());
         new File(crashDir).mkdirs();
+        //File crashFile = new File(crashDir, name);
         for (int i = 0; i < request.getCrashReportDataCount(); i++) {
             File crashFile = new File(crashDir, name + "_" + (i + 1));
             try {
-                Files.write(crashFile.toPath(),
-                            request.getCrashReportData(i).toByteArray(),
-                            StandardOpenOption.CREATE,
-                            StandardOpenOption.TRUNCATE_EXISTING);
+                Files.write(crashFile.toPath(), request.getCrashReportData(i).toByteArray(),
+                            StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
             } catch (IOException e) {
                 logger.log(WARNING, "Couldn't write crash file to " + crashFile, e);
                 response.onNext(CrashReportResponse.newBuilder().setResult(Status.FAILED).build());
